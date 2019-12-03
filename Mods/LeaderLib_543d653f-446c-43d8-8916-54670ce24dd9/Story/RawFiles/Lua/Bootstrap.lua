c4f5b1e96f846fb6ade60c97805e96bb9c71cd76
LaughingLeader <laughingleader@gmail.com>
2019-12-03 11:11:15 -0600

    Testing custom lua queries/calls, got StringToVersion to work properly

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Bootstrap.lua
@@ -1,2 +1,4 @@
 Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "leaderLib_ext_main.lua");
 
+Ext.NewQuery(leaderlib.string_to_version, "LeaderLib_Ext_StringToVersion", "[in](STRING)_Version, [out](INTEGER)_Major, [out](INTEGER)_Minor, [out](INTEGER)_Revision, [out](INTEGER)_Build");
+Ext.NewCall(leaderlib.print_attributes, "LeaderLib_Ext_PrintAttributes", "(CHARACTERGUID)_Char");
--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/leaderLib_ext_main.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/leaderLib_ext_main.lua
@@ -7,50 +7,51 @@ local function table_has_index(tbl, index)
     return false
 end
 
-local function string_to_version(version_str)
-	obj = nil
-	if ObjectExists("S_LeaderLib_EventParser_6983a226-0d86-47da-a27f-ee3e483625e6") == 1 then
-		obj = "S_LeaderLib_EventParser_6983a226-0d86-47da-a27f-ee3e483625e6"
-	else
-		obj = CharacterGetHostCharacter()
+local function process_version_str(version_str)
+	local a,b,c,d = -1
+	vals = {}
+	for s in string.gmatch(version_str, "([^.]+)") do
+		table.insert(vals, tonumber(s))
 	end
+	for k,v in pairs(vals) do
+		if k == 1 then
+			a = v
+		elseif k == 2 then
+			b = v
+		elseif k == 3 then
+			c = v
+		elseif k == 4 then
+			d = v
+			break
+		end
+	end
+	return a,b,c,d
+end
 
-	if obj ~= nil then
-		version_vals = {}
-		for s in string.gmatch(version_str, "([^.]+)") do
-			table.insert(version_vals, s)
-		end
-		--for i,v in ipairs(version_vals) do print(i,v) end
-
-		major = -1
-		minor = -1
-		revision = -1
-		build = -1
-		if table_has_index(version_vals, 0) then
-			major = version_vals[0]
-		end
-		if table_has_index(version_vals, 1) then
-			minor = version_vals[1]
-		end
-		if table_has_index(version_vals, 2) then
-			revision = version_vals[2]
-		end
-		if table_has_index(version_vals, 3) then
-			build = version_vals[3]
-		end
+local function string_to_version(version_str)
+	local b, major,minor,revision,build = pcall(process_version_str, version_str)
+	if b then
 		if major ~= -1 and minor ~= -1 and revision ~= -1 and build ~= -1 then
-			-- SetVarInteger(obj, "LeaderLib_StringToVersion_Major", major)
-			-- SetVarInteger(obj, "LeaderLib_StringToVersion_Minor", minor)
-			-- SetVarInteger(obj, "LeaderLib_StringToVersion_Revision", revision)
-			-- SetVarInteger(obj, "LeaderLib_StringToVersion_Build", build)
-			-- ObjectSetFlag(obj, "LeaderLib_StringToVersion_Success", 0)
-			-- SetStoryEvent(obj, "LeaderLib_StringToVersion_Success")
-			--LeaderLib_StringExt_SetVersionFromString(version_str, major, minor, revision, build)
-			--Osi.DB_LeaderLib_StringExt_VersionToInt(version_str, major, minor, revision, build)
+			--Osi.LeaderLib_StringExt_SetVersionFromString(version_str,major,minor,revision,build)
 			return major,minor,revision,build
-		else 
-			return nil
+		else
+			error(version_str .. " is not a valid version string!")
 		end
+	else
+		error("Failed to process '"..version_str.."' - function process_version_str has an error.")
+	end
+end
+
+function LeaderLib_Ext_StringToVersion(version_str)
+	local b, major,minor,revision,build = pcall(process_version_str, version_str)
+	if b then
+		if major ~= -1 and minor ~= -1 and revision ~= -1 and build ~= -1 then
+			Osi.LeaderLib_StringExt_SetVersionFromString(version_str,major,minor,revision,build)
+		else
+			error(version_str .. " is not a valid version string!")
+		end
+	else
+		error("Failed to process '"..version_str.."' - function process_version_str has an error.")
 	end
 end
 
@@ -66,6 +67,3 @@ leaderlib = {
 	string_to_version = string_to_version,
 	print_attributes = print_attributes
 }
-
---Ext.NewQuery(leaderlib.string_to_version, "LeaderLib_Ext_StringToVersion", "[in](STRING)_Version, [out](INTEGER)_Major, [out](INTEGER)_Minor, [out](INTEGER)_Revision, [out](INTEGER)_Build");
---Ext.NewCall(leaderlib.print_attributes, "LeaderLib_Ext_PrintAttributes", "(CHARACTERGUID)_Char");

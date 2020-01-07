28337c08e9fdc753aa37a608a1e821981b91b179
LaughingLeader <laughingleader@gmail.com>
2020-01-06 21:37:44 -0600

    Reworked skill refreshing using the new skill iterator

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/LeaderLib_GameMechanics.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/LeaderLib_GameMechanics.lua
@@ -43,7 +43,7 @@ local function RedirectDamage(blocker, target, attacker, handlestr, reduction_st
     local redirected_hit = NRD_HitPrepare(blocker, attacker)
     local damageRedirected = false
 
-    for k,v in pairs(_G["LeaderLib"].Data["DamageTypes"]) do
+    for k,v in pairs(LeaderLib.Data.DamageTypes) do
         local damage = NRD_HitStatusGetDamage(target, handle, v)
         if damage ~= nil and damage > 0 then
             local reduced_damage = math.max(math.ceil(damage * reduction), 1)
@@ -71,38 +71,41 @@ local function RedirectDamage(blocker, target, attacker, handlestr, reduction_st
 	return damageRedirected;
 end
 
-local function RefreshSkills(char)
+---Get a skill's slot and cooldown, and store it in DB_LeaderLib_Helper_Temp_RefreshUISkill.
+---@param char string
+---@param skill string
+local function StoreSkillData(char, skill)
+    local slot = NRD_SkillBarFindSkill(char, skill)
+    if slot ~= nil then
+        local success,cd = pcall(NRD_SkillGetCooldown, char, skill)
+        if success == false or cd == nil then cd = 0.0; end
+        cd = math.max(cd, 0.0)
+        --Osi.LeaderLib_RefreshUI_Internal_StoreSkillData(char, skill, slot, cd)
+        Osi.DB_LeaderLib_Helper_Temp_RefreshUISkill(char, skill, slot, cd)
+        NRD_SkillBarClear(char, slot)
+        Osi.LeaderLog_Log("DEBUG", "[lua:LeaderLib_RefreshSkill] Refreshing (" .. tostring(skill) ..") for (" .. tostring(char) .. ") [" .. tostring(cd) .. "]")
+    end
+ end
+
+ local function StoreSkillSlots(char)
 	-- Until we can fetch the active skill bar, iterate through every skill slot for now
    for i=0,144 do
 	   local skill = NRD_SkillBarGetSkill(char, i)
 	   if skill ~= nil then
 		   local success,cd = pcall(NRD_SkillGetCooldown, char, skill)
-		   if success == false then cd = 0.0 end;
+		   if success == false or cd == nil then cd = 0.0 end;
 		   cd = math.max(cd, 0.0)
 		   Osi.LeaderLib_RefreshUI_Internal_StoreSkillData(char, skill, i, cd)
-		   Osi.LeaderLog_Log("DEBUG", "[lua:LeaderLib_RefreshSkills] Refreshing (" .. tostring(skill) ..") for (" .. tostring(char) .. ") [" .. tostring(cd) .. "]")
+		   Osi.LeaderLog_Log("DEBUG", "[lua:LeaderLib_RefreshSkills] Storing skill slot data (" .. tostring(skill) ..") for (" .. tostring(char) .. ") [" .. tostring(cd) .. "]")
 	   end
    end
-   Osi.LeaderLib_Timers_StartObjectTimer(char, 60, "Timers_LeaderLib_RefreshUI_RevertSkillCooldown", "LeaderLib_RefreshUI_RevertSkillCooldown");
-end
-
-local function RefreshSkill(char, skill)
-   local slot = NRD_SkillBarFindSkill(char, skill)
-   if slot ~= nil then
-	   local cd = pcall(NRD_SkillGetCooldown, char, skill)
-	   if cd == nil then cd = 0 end
-	   cd = math.max(cd, 0)
-	   Osi.LeaderLib_RefreshUI_Internal_StoreSkillData(char, skill, slot, cd)
-	   Osi.LeaderLog_Log("DEBUG", "[lua:LeaderLib_RefreshSkill] Refreshing (" .. tostring(skill) ..") for (" .. tostring(char) .. ") [" .. tostring(cd) .. "]")
-   end
-   Osi.LeaderLib_Timers_StartObjectTimer(char, 60, "Timers_LeaderLib_RefreshUI_RevertSkillCooldownDirect", "LeaderLib_RefreshUI_RevertSkillCooldown");
 end
 
 LeaderLib.Game = {
 	ReduceDamage = ReduceDamage,
 	RedirectDamage = RedirectDamage,
-	RefreshSkills = RefreshSkills,
-	RefreshSkill = RefreshSkill,
+    StoreSkillData = StoreSkillData,
+    StoreSkillSlots = StoreSkillSlots,
 }
 
 LeaderLib.Register.Table(LeaderLib.Game);

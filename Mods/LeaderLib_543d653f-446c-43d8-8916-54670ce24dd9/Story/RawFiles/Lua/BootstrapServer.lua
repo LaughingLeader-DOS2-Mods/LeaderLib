2dcff4ce7c52bb9f4e959ff1d19fdf1fa29134e0
LaughingLeader <laughingleader@gmail.com>
2020-03-27 08:38:56 -0500

    Fix for lua scripts not loading when in pak form (extender path separator bug)

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapServer.lua
@@ -9,11 +9,11 @@
 -- Ext.RegisterListener("SessionLoading", LeaderLib_GameSessionLoad)
 -- Ext.RegisterListener("ModuleLoading", LeaderLib_ModuleLoading)
 
-Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Shared\\LeaderLib_Common.lua")
+Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Shared/LeaderLib_Common.lua")
 
-Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server\\LeaderLib_Main.lua")
-Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server\\LeaderLib_Versioning.lua")
-Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server\\LeaderLib_Statuses.lua")
-Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server\\LeaderLib_GameMechanics.lua")
-Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server\\LeaderLib_GlobalSettings.lua")
-Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server\\LeaderLib_Debug.lua")
+Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server/LeaderLib_Main.lua")
+Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server/LeaderLib_Versioning.lua")
+Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server/LeaderLib_Statuses.lua")
+Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server/LeaderLib_GameMechanics.lua")
+Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server/LeaderLib_GlobalSettings.lua")
+Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Server/LeaderLib_Debug.lua")

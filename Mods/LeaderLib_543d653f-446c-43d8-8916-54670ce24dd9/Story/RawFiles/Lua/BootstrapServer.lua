6d93ad6e3cc1654060496cc598f782b4ec0042c9
LaughingLeader <laughingleader@gmail.com>
2020-03-27 06:03:56 -0500

    Disabled empty listener functions for now

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapServer.lua
@@ -1,20 +1,13 @@
-local function LeaderLib_GameSessionLoad()
-	Ext.Print("[LeaderLib:Bootstrap.lua] Session is loading.")
-end
+-- local function LeaderLib_GameSessionLoad()
+-- 	Ext.Print("[LeaderLib:Bootstrap.lua] Session is loading.")
+-- end
 
-local function LeaderLib_ModuleLoading()
-	Ext.Print("[LeaderLib:Bootstrap.lua] Module is loading.")
-	-- local loadOrder = Ext.GetModLoadOrder();
-	-- Ext.Print("[LeaderLib:Bootstrap.lua] Mod Order:")
-	-- for i=1,#loadOrder do
-	-- 	local uuid = loadOrder[i]
-	-- 	local mod = Ext.GetModInfo(uuid)
-	-- 	Ext.Print("  " .. tostring(i) .. ". " .. mod.Name .. "(".. uuid .. ")")
-	-- end
-end
+-- local function LeaderLib_ModuleLoading()
+-- 	Ext.Print("[LeaderLib:Bootstrap.lua] Module is loading.")
+-- end
 
-Ext.RegisterListener("SessionLoading", LeaderLib_GameSessionLoad)
-Ext.RegisterListener("ModuleLoading", LeaderLib_ModuleLoading)
+-- Ext.RegisterListener("SessionLoading", LeaderLib_GameSessionLoad)
+-- Ext.RegisterListener("ModuleLoading", LeaderLib_ModuleLoading)
 
 Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "Shared\\LeaderLib_Common.lua")
 

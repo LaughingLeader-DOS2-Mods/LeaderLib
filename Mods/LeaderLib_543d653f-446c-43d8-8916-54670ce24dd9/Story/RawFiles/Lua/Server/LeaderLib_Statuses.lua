2186a1e3a2096ea9a7827a5d974305189d0969e4
LaughingLeader <laughingleader@gmail.com>
2020-04-11 11:08:49 -0500

    v43 release update

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/LeaderLib_Statuses.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/LeaderLib_Statuses.lua
@@ -3,13 +3,13 @@
 ---@return boolean
 function LeaderLib_Ext_IsSneakingOrInvisible(obj)
     if HasActiveStatus(obj, "SNEAKING") == 1 or HasActiveStatus(obj, "INVISIBLE") == 1 then
-        return 1
+        return true
 	else
 		local invisibleTable = LeaderLib.StatusTypes["INVISIBLE"]
 		if invisibleTable ~= nil then
 			for status,b in pairs(invisibleTable) do
 				if HasActiveStatus(obj, status) == 1 then
-					return 1
+					return true
 				end
 			end
 		end
@@ -27,7 +27,7 @@ function LeaderLib_Ext_IsSneakingOrInvisible(obj)
         --     end
         -- end
     end
-    return 0
+    return false
 end
 
 Ext.NewQuery(LeaderLib_Ext_IsSneakingOrInvisible, "LeaderLib_Ext_QRY_IsSneakingOrInvisible", "[in](GUIDSTRING)_Object, [out](INTEGER)_Bool")
@@ -40,19 +40,19 @@ function LeaderLib_Ext_HasStatusType(obj, statusType)
 	if statusType ~= nil and statusType ~= "" then
 		statusType = string.upper(statusType)
 		if HasActiveStatus(obj, statusType) == 1 then
-			return 1
+			return true
 		else
 			local statusTypeTable = LeaderLib.StatusTypes[statusType]
 			if statusTypeTable ~= nil then
 				for status,b in pairs(statusTypeTable) do
 					if HasActiveStatus(obj, status) == 1 then
-						return 1
+						return true
 					end
 				end
 			end
 		end
 	end
-    return 0
+    return false
 end
 
 Ext.NewQuery(LeaderLib_Ext_HasStatusType, "LeaderLib_Ext_QRY_HasStatusType", "[in](GUIDSTRING)_Object, [in](STRING)_StatusType, [out](INTEGER)_Bool")

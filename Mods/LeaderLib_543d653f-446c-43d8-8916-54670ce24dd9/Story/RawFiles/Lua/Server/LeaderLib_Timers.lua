7550e3208e1de41499089b11b079d676c6b24343
LaughingLeader <laughingleader@gmail.com>
2020-04-17 03:05:20 -0500

    Added Lua-based timers

---
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/LeaderLib_Timers.lua
@@ -0,0 +1,39 @@
+local function TryStartTimer(event, delay, ...)
+	local timerName = event
+	local uuids = {...}
+	local paramCount = #uuids
+	if uuids == nil or paramCount == 0 then
+		Osi.LeaderLib_Timers_Internal_StoreLuaData(timerName, event)
+	else
+		if paramCount == 1 then
+			timerName = event..uuids[1]
+			Osi.LeaderLib_Timers_Internal_StoreLuaData(timerName, event, uuids[1])
+		elseif paramCount >= 2 then
+			timerName = event..uuids[1]..uuids[2]
+			Osi.LeaderLib_Timers_Internal_StoreLuaData(timerName, event, uuids[1], uuids[2])
+		end
+	end
+	TimerCancel(timerName)
+	TimerLaunch(timerName, delay)
+end
+
+---Starts an Osiris timer with a variable amount of UUIDs (or none).
+---@param event string
+---@param delay integer
+function LeaderLib_Ext_StartTimer(event, delay, ...)
+	local status,err = xpcall(TryStartTimer, debug.traceback, delay, ...)
+	if not status then
+		Ext.PrintError("Error starting timer:\n", err)
+	end
+end
+
+function LeaderLib_Ext_TimerFinished(event, ...)
+	if #LeaderLib.Listeners.TimerFinished > 0 then
+		for i,callback in ipairs(LeaderLib.Listeners.TimerFinished) do
+			local status,err = xpcall(callback, debug.traceback, event, ...)
+			if not status then
+				Ext.PrintError("Error sending timer finished event:\n", err)
+			end
+		end
+	end
+end

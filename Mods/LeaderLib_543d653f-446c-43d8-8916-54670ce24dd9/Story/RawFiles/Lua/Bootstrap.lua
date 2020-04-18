dcbbcc328e49cc0bdc4cbe1c5cf405f26559e4e4
LaughingLeader <laughingleader@gmail.com>
2020-04-18 01:07:11 -0500

    Removed deprecated script

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Bootstrap.lua
+++
@@ -1,4 +0,0 @@
-if Ext.Version() < 42 then
-	Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "BootstrapClient.lua")
-	Ext.Require("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c", "BootstrapServer.lua")
-end

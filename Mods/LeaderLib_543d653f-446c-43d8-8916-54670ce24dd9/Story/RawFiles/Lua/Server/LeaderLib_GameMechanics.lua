760cc0c5239e4bcc89e44086e18441eae6f3c712
LaughingLeader <laughingleader@gmail.com>
2020-05-09 18:56:51 -0500

    Added a lua helper for exploding at a position

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/LeaderLib_GameMechanics.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/LeaderLib_GameMechanics.lua
@@ -298,18 +298,35 @@ local function ExplodeProjectile(source, target, skill)
         SetStoryEvent(source, "LeaderLib_Commands_SetItemLevel")
         level = GetVarInteger(source, "LeaderLib_Level")
     end
-    local x,y,z = GetPosition(target)
-    NRD_ProjectilePrepareLaunch();
-    NRD_ProjectileSetString("SkillId", skill);
-    NRD_ProjectileSetInt("CasterLevel", level);
-    NRD_ProjectileSetGuidString("SourcePosition", target);
-    NRD_ProjectileSetGuidString("Caster", source);
-    NRD_ProjectileSetGuidString("Source", source);
-    NRD_ProjectileSetGuidString("HitObject", target);
-    NRD_ProjectileSetGuidString("HitObjectPosition", target);
-    NRD_ProjectileSetGuidString("TargetPosition", target);
-    NRD_ProjectileLaunch();
-    PrintDebug("Exploded projectile ("..skill..") at ("..target..")")
+    NRD_ProjectilePrepareLaunch()
+    NRD_ProjectileSetString("SkillId", skill)
+    NRD_ProjectileSetInt("CasterLevel", level)
+    NRD_ProjectileSetGuidString("SourcePosition", target)
+    NRD_ProjectileSetGuidString("Caster", source)
+    NRD_ProjectileSetGuidString("Source", source)
+    NRD_ProjectileSetGuidString("HitObject", target)
+    NRD_ProjectileSetGuidString("HitObjectPosition", target)
+    NRD_ProjectileSetGuidString("TargetPosition", target)
+    NRD_ProjectileLaunch()
+end
+
+local function ExplodeProjectileAtPosition(source, skill, x, y, z)
+    local level = 1
+    if ObjectIsCharacter(source) == 1 then
+        level = CharacterGetLevel(source)
+    else
+        SetStoryEvent(source, "LeaderLib_Commands_SetItemLevel")
+        level = GetVarInteger(source, "LeaderLib_Level")
+    end
+    NRD_ProjectilePrepareLaunch()
+    NRD_ProjectileSetString("SkillId", skill)
+    NRD_ProjectileSetInt("CasterLevel", level)
+    NRD_ProjectileSetGuidString("SourcePosition", target)
+    NRD_ProjectileSetGuidString("Caster", source)
+    NRD_ProjectileSetGuidString("Source", source)
+    NRD_ProjectileSetVector3("HitObjectPosition", x,y,z)
+    NRD_ProjectileSetVector3("TargetPosition", x,y,z)
+    NRD_ProjectileLaunch()
 end
 
 --Ext.NewCall(ExplodeProjectile, "LeaderLib_Ext_ExplodeProjectile", "(GUIDSTRING)_Source, (GUIDSTRING)_Target, (STRING)_Skill")
@@ -368,4 +385,5 @@ Game.TrySetSkillSlot = TrySetSkillSlot
 Game.RefreshSkill = RefreshSkill
 Game.CloneItemForCharacter = CloneItemForCharacter
 Game.ExplodeProjectile = ExplodeProjectile
+Game.ExplodeProjectileAtPosition = ExplodeProjectileAtPosition
 Game.RemoveRunes = RemoveRunes

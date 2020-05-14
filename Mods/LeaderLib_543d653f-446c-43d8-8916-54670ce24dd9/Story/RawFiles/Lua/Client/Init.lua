2954952411a6cc9bcc137d5e65c363d621bd9512
LaughingLeader <laughingleader@gmail.com>
2020-05-14 05:45:40 -0500

    Moved tooltip helpers

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/Init.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/Init.lua
@@ -1,6 +1,13 @@
+if UI == nil then
+UI = {
+	Tooltip = {}
+}
+end
+
 Ext.Require("Client/UI/CharacterSheet.lua")
 Ext.Require("Client/UI/ModMenu.lua")
 Ext.Require("Client/UI/Tooltip.lua")
 Ext.Require("Client/UI/Debug.lua")
+Ext.Require("Client/UI/TooltipHelpers.lua")
 Ext.Require("Client/ClientNetMessages.lua")
 Ext.RegisterListener("SessionLoaded", RegisterListeners)

08f7629db9458b0d64679833f965f2238d149e3d
LaughingLeader <laughingleader@gmail.com>
2020-05-29 14:26:41 -0500

    Tweak to ShootProjectile to support objects and positions in the same function

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Game/ProjectileHelpers.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Game/ProjectileHelpers.lua
@@ -1,4 +1,4 @@
-local function ShootProjectile(source, target, skill, forceHit)
+local function ShootProjectile(source, target, skill, forceHit, sourcePosition)
     local level = 1
     if ObjectIsCharacter(source) == 1 then
         level = CharacterGetLevel(source)
@@ -12,15 +12,25 @@ local function ShootProjectile(source, target, skill, forceHit)
 
     NRD_ProjectileSetGuidString("Caster", source)
     NRD_ProjectileSetGuidString("Source", source)
-    if forceHit == true then
-        NRD_ProjectileSetGuidString("SourcePosition", source)
-        NRD_ProjectileSetGuidString("HitObject", target)
-        NRD_ProjectileSetGuidString("HitObjectPosition", target)
-    else
-        local x,y,z = GetPosition(source)
+    local x,y,z = GetPosition(source)
+    if sourcePosition == nil then
         NRD_ProjectileSetVector3("SourcePosition", x,y+2,z)
+    else
+        NRD_ProjectileSetVector3("SourcePosition", sourcePosition[1],sourcePosition[2],sourcePosition[3])
     end
-    NRD_ProjectileSetGuidString("TargetPosition", target)
+    if type(target) == "string" then
+        if forceHit == true then
+            NRD_ProjectileSetGuidString("HitObject", target)
+            NRD_ProjectileSetGuidString("HitObjectPosition", target)
+        end
+        NRD_ProjectileSetGuidString("TargetPosition", target)
+    elseif type(target) == "table" then
+        local tx,ty,tz = table.unpack(target)
+        if forceHit == true then
+            NRD_ProjectileSetVector3("HitObjectPosition", tx,ty,tz)
+        end
+        NRD_ProjectileSetVector3("TargetPosition", tx,ty,tz)
+    end
     NRD_ProjectileLaunch()
 end
 GameHelpers.ShootProjectile = ShootProjectile
@@ -93,3 +103,40 @@ local function ExplodeProjectileAtPosition(source, skill, x, y, z)
     NRD_ProjectileLaunch()
 end
 GameHelpers.ExplodeProjectileAtPosition = ExplodeProjectileAtPosition
+
+local function GetForwardPosition(source, distanceMult)
+    local x,y,z = GetPosition(source)
+    local character = Ext.GetCharacter(source)
+    if character ~= nil then
+        if distanceMult == nil then
+            distanceMult = 1.0
+        end
+        local forwardVector = {
+            -character.Stats.Rotation[7] * distanceMult,
+            0,---rot[8] * distanceMult, -- Rot Y is never used since objects can't look "up"
+            -character.Stats.Rotation[9] * distanceMult,
+        }
+        x = character.Stats.Position[1] + forwardVector[1]
+        z = character.Stats.Position[3] + forwardVector[3]
+    end
+    return {x,y,z}
+end
+GameHelpers.GetForwardPosition = GetForwardPosition
+
+local function ExtendPositionWithForwardDirection(source, distanceMult, x,y,z)
+    local character = Ext.GetCharacter(source)
+    if character ~= nil then
+        if distanceMult == nil then
+            distanceMult = 1.0
+        end
+        local forwardVector = {
+            -character.Stats.Rotation[7] * distanceMult,
+            0,---rot[8] * distanceMult, -- Rot Y is never used since objects can't look "up"
+            -character.Stats.Rotation[9] * distanceMult,
+        }
+        x = x + forwardVector[1]
+        z = z + forwardVector[3]
+    end
+    return {x,y,z}
+end
+GameHelpers.ExtendPositionWithForward = ExtendPositionWithForwardDirection

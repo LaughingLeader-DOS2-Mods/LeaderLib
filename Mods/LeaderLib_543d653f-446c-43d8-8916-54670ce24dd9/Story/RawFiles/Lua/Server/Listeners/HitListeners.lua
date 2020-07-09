6d79862338500fe9b12513180d90aa2dfc8b3602
LaughingLeader <laughingleader@gmail.com>
2020-07-09 03:48:39 -0500

    Renamed HitListeners, added new Osiris listenrs to test

---
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Listeners/HitListeners.lua
@@ -0,0 +1,75 @@
+---@type target string
+---@type source string
+---@type damage integer
+---@type handle integer
+local function OnPrepareHit(target, source, damage, handle)
+	if #Listeners.OnPrepareHit > 0 then
+		for i,callback in ipairs(Listeners.OnPrepareHit) do
+			local status,err = xpcall(callback, debug.traceback, target, source, damage, handle)
+			if not status then
+				Ext.PrintError("[LeaderLib:HitListeners.lua] Error calling function for 'OnPrepareHit':\n", err)
+			end
+		end
+	end
+
+	if damage > 0 and ObjectIsCharacter(source) == 1 and 
+		GameHelpers.HasTagEquipped(source, "LeaderLib_HasResistancePenetration") then
+			SaveDamageAmountForResistancePenetration(target, source, damage, handle)
+	end
+end
+
+Ext.RegisterOsirisListener("NRD_OnPrepareHit", 4, "after", OnPrepareHit)
+
+---@type target string
+---@type source string
+---@type damage integer
+---@type handle integer
+local function BeforeOnHit(target, source, damage, handle)
+	print("BeforeOnHit",target, source, damage, handle)
+	--- Apply this before other mods possibly mess with the damage.
+	ApplyResistancePenetration(target, source, damage, handle)
+end
+Ext.RegisterOsirisListener("NRD_OnHit", 4, "after", BeforeOnHit)
+
+
+---@type target string
+---@type source string
+---@type damage integer
+---@type handle integer
+local function OnHit(target, source, damage, handle)
+	local skillprototype = NRD_StatusGetString(target, handle, "SkillId")
+	if skillprototype ~= "" and skillprototype ~= nil then
+		OnSkillHit(source, skillprototype, target, handle, damage)
+	end
+
+	if source ~= nil and Features.ApplyBonusWeaponStatuses == true and GameHelpers.HitWithWeapon(target, handle, nil, nil, source) then
+		--PrintDebug("Basic Attack Hit on", target, ". Checking for statuses with a BonusWeapon")
+		---@type EsvCharacter
+		local character = Ext.GetCharacter(source)
+		for i,status in pairs(character:GetStatuses()) do
+			if NRD_StatAttributeExists(status, "StatsId") == 1 then
+				local potion = Ext.StatGetAttribute(status, "StatsId")
+				if potion ~= nil and potion ~= "" then
+					local bonusWeapon = Ext.StatGetAttribute(potion, "BonusWeapon")
+					if bonusWeapon ~= nil and bonusWeapon ~= "" then
+						local extraProps = Ext.StatGetAttribute(bonusWeapon, "ExtraProperties")
+						if extraProps ~= nil then
+							GameHelpers.ApplyProperties(target, source, extraProps)
+						end
+					end
+				end
+			end
+		end
+	end
+
+	if #Listeners.OnHit > 0 then
+		for i,callback in ipairs(Listeners.OnHit) do
+			local status,err = xpcall(callback, debug.traceback, target, source, damage, handle)
+			if not status then
+				Ext.PrintError("[LeaderLib:HitListeners.lua] Error calling function for 'OnHit':\n", err)
+			end
+		end
+	end
+end
+Ext.NewCall(OnHit, "LeaderLib_Ext_OnHit", "(GUIDSTRING)_Target, (GUIDSTRING)_Source, (INTEGER)_Damage, (INTEGER64)_Handle")
+--Ext.RegisterOsirisListener("NRD_OnHit", 4, "after", OnHit)

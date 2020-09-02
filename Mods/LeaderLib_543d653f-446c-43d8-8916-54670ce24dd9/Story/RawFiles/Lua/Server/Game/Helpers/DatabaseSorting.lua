896999b7559b867433b04afa7b247ce7865acdf4
LaughingLeader <laughingleader@gmail.com>
2020-09-02 14:46:29 -0500

    Optimized db/dictionary sorting with a new lua function

---
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Game/Helpers/DatabaseSorting.lua
@@ -0,0 +1,59 @@
+if GameHelpers.DB == nil then
+	GameHelpers.DB = {}
+end
+
+local function GetArity(arity)
+	local nilColumns = {}
+	for i=0,arity do
+		table.insert(nilColumns, "nil")
+	end
+	return table.unpack(nilColumns)
+end
+
+local function SortDatabase(name, arity, sortColumn)
+	local b,result = xpcall(function()
+		local db = Osi[name]:Get(GetArity(arity))
+		if db ~= nil then
+			table.sort(db, function(a,b)
+				return a[sortColumn] < b[sortColumn]
+			end)
+		end
+		return db
+	end, debug.traceback)
+	if not b then
+		Ext.PrintError("[LeaderLib:GameHelpers.DB.SortDatabase] Error sorting database:", name)
+		Ext.PrintError(result)
+	else
+		if result ~= nil then
+			Osi[name]:Delete(GetArity(arity))
+			for i,v in pairs(result) do
+				Osi[name][i](v)
+			end
+			--PrintDebug(name, Ext.JsonStringify(Osi[name]:Get(GetArity(arity))))
+		end
+	end
+end
+GameHelpers.DB.SortDatabase = SortDatabase
+Ext.NewCall(SortDatabase, "LeaderLib_Ext_SortDatabase", "(STRING)_DatabaseName, (INTEGER)_Arity, (INTEGER)_SortByColumn")
+
+function SortDictionary(id)
+	local b,result = xpcall(function()
+		--DB_LeaderLib_Dictionary_Data(_DictionaryID, _Index, _CheckID, _CheckDisplayName)
+		local db = Osi.DB_LeaderLib_Dictionary_Data:Get(id, nil, nil, nil)
+		if db ~= nil then
+			table.sort(db, function(a,b)
+				return a[4] < b[4]
+			end)
+		end
+		Osi.DB_LeaderLib_Dictionary_Data:Delete(id, nil, nil, nil)
+		for i,v in pairs(db) do
+			Osi.DB_LeaderLib_Dictionary_Data(id, v[2], v[3], v[4])
+		end
+		--PrintDebug(id, Ext.JsonStringify(Osi.DB_LeaderLib_Dictionary_Data:Get(id, nil, nil, nil)))
+		return true
+	end, debug.traceback)
+	if not b then
+		Ext.PrintError("[LeaderLib:SortDictionary] Error sorting dictionary:", id)
+		Ext.PrintError(result)
+	end
+end

726d685b95c328c9095b1fe7856da3fbab2eb741
LaughingLeader <laughingleader@gmail.com>
2020-09-29 11:11:24 -0500

    Added listeners for switching characters, to updated SharedData

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Helpers/SharedDataManager.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Helpers/SharedDataManager.lua
@@ -46,13 +46,10 @@ if Ext.IsClient() then
 	SharedData.IsHost = false
 	SharedData.ID = -1
 	SharedData.Profile = ""
-	SharedData.CurrentCharacter = function()
-		return SharedData.CharacterData[SharedData.Profile]		
-	end
 end
 
 if Ext.IsServer() then
-	function GameHelpers.Data.SyncSharedData(client)
+	function GameHelpers.Data.SyncSharedData(client, ignoreProfile, skipSettingsSync)
 		if #Listeners.SyncData > 0 then
 			for i,callback in pairs(Listeners.SyncData) do
 				local status,result = xpcall(callback, debug.traceback, SharedData.ModData, client)
@@ -64,33 +61,59 @@ if Ext.IsServer() then
 		if client == nil then
 			--Ext.BroadcastMessage("LeaderLib_SharedData_StoreData", Ext.JsonStringify(SharedData), nil)
 			for id,b in pairs(UserIds) do
-				local isHost = StringHelpers.GetUUID(CharacterGetHostCharacter()) == StringHelpers.GetUUID(GetCurrentCharacter(id))
-				local data = {
-					RegionData = SharedData.RegionData, 
-					CharacterData = SharedData.CharacterData, 
-					ModData = SharedData.ModData, 
-					ID = id, 
-					Profile = GetUserProfileID(id),
-					IsHost = isHost
-				}
-				Ext.PostMessageToUser(id, "LeaderLib_SharedData_StoreData", Ext.JsonStringify(data))
+				local profile = GetUserProfileID(id)
+				if profile ~= ignoreProfile then
+					local isHost = StringHelpers.GetUUID(CharacterGetHostCharacter()) == StringHelpers.GetUUID(GetCurrentCharacter(id))
+					local data = {
+						RegionData = SharedData.RegionData, 
+						CharacterData = SharedData.CharacterData, 
+						ModData = SharedData.ModData, 
+						ID = id, 
+						Profile = profile,
+						IsHost = isHost
+					}
+					Ext.PostMessageToUser(id, "LeaderLib_SharedData_StoreData", Ext.JsonStringify(data))
+				end
 			end
 		else
-			local id = CharacterGetReservedUserID(client)
-			local profile = GetUserProfileID(id)
-			local isHost = StringHelpers.GetUUID(CharacterGetHostCharacter()) == StringHelpers.GetUUID(GetCurrentCharacter(id))
-			local data = {
-				RegionData = SharedData.RegionData, 
-				CharacterData = SharedData.CharacterData, 
-				ModData = SharedData.ModData, 
-				ID = id, 
-				Profile = GetUserProfileID(id),
-				IsHost = isHost
-			}
-			Ext.PostMessageToUser(id, "LeaderLib_SharedData_StoreData", Ext.JsonStringify(data))
+			local clientType = type(client)
+			if clientType == "string" then
+				local id = CharacterGetReservedUserID(client)
+				local profile = GetUserProfileID(id)
+				if profile ~= ignoreProfile then
+					local isHost = StringHelpers.GetUUID(CharacterGetHostCharacter()) == StringHelpers.GetUUID(GetCurrentCharacter(id))
+					local data = {
+						RegionData = SharedData.RegionData, 
+						CharacterData = SharedData.CharacterData, 
+						ModData = SharedData.ModData, 
+						ID = id, 
+						Profile = profile,
+						IsHost = isHost
+					}
+					Ext.PostMessageToUser(id, "LeaderLib_SharedData_StoreData", Ext.JsonStringify(data))
+				end
+			elseif clientType == "number" then
+				local profile = GetUserProfileID(client)
+				if profile ~= ignoreProfile then
+					local isHost = StringHelpers.GetUUID(CharacterGetHostCharacter()) == StringHelpers.GetUUID(GetCurrentCharacter(client))
+					local data = {
+						RegionData = SharedData.RegionData, 
+						CharacterData = SharedData.CharacterData, 
+						ModData = SharedData.ModData, 
+						ID = client, 
+						profile,
+						IsHost = isHost
+					}
+					Ext.PostMessageToClient(client, "LeaderLib_SharedData_StoreData", Ext.JsonStringify(data))
+				end
+			else
+				Ext.PrintError("[LeaderLib:GameHelpers.Data.SyncSharedData] Error syncing data: client is an incorrect type:", clientType, client)
+			end
 		end
-		--SettingsManager.Sync()
-		SettingsManager.SyncAllSettings(client)
+		if skipSettingsSync ~= true then
+			--SettingsManager.Sync()
+			SettingsManager.SyncAllSettings(client)
+		end
 	end
 
 	local function OnSyncTimer()
@@ -164,9 +187,25 @@ if Ext.IsServer() then
 			GameHelpers.Data.StartSyncTimer()
 		end
 	end)
+
+	Ext.RegisterNetListener("LeaderLib_SharedData_CharacterSelected", function(cmd, payload)
+		local data = Ext.JsonParse(payload)
+		local profile = data.Profile
+		local uuid = data.UUID
+		if profile ~= nil and uuid ~= nil and SharedData.CharacterData[profile] ~= nil then
+			SharedData.CharacterData[profile].UUID = uuid
+			GameHelpers.Data.SyncSharedData(nil, profile, true)
+		end
+	end)
 end
 
 if Ext.IsClient() then
+	local function GetClientCharacter()
+		if SharedData.CharacterData ~= nil and SharedData.Profile ~= nil then
+			return SharedData.CharacterData[SharedData.Profile]	
+		end
+	end
+
 	Ext.RegisterNetListener("LeaderLib_SharedData_StoreData", function(cmd, payload)
 		SharedData = Ext.JsonParse(payload)
 		if #Listeners.ClientDataSynced > 0 then
@@ -175,6 +214,54 @@ if Ext.IsClient() then
 				if not status then
 					Ext.PrintError("Error calling function for 'ClientDataSynced':\n", err)
 				end
+			end
+		end
+	end)
+
+	local function OnCharacterSelected(ui, call, doubleHandle, skipSync)
+		--print(ui:GetTypeId(), call, doubleHandle)
+		local handle = Ext.DoubleToHandle(doubleHandle)
+		if handle ~= nil then
+			local character = Ext.GetCharacter(handle)
+			if character ~= nil then
+				local currentCharacter = GetClientCharacter()
+				if currentCharacter ~= nil then
+					--print(currentCharacter.UUID, "=>", character.MyGuid)
+					currentCharacter.UUID = character.MyGuid
+				end
+				if skipSync ~= true then
+					Ext.PostMessageToServer("LeaderLib_SharedData_CharacterSelected", Ext.JsonStringify({Profile = SharedData.Profile, UUID = character.MyGuid}))
+				end
+			end
+		end
+	end
+
+	local lastCharacterOutsideTrade = ""
+
+	Ext.RegisterListener("SessionLoaded", function()
+		Ext.RegisterUITypeCall(Data.UIType.playerInfo, "charSel", OnCharacterSelected)
+		Ext.RegisterUITypeCall(Data.UIType.characterSheet, "selectCharacter", OnCharacterSelected)
+		Ext.RegisterUITypeCall(Data.UIType.trade, "selectCharacter", function(ui, call, doubleHandle)
+			OnCharacterSelected(ui, call, doubleHandle, true)
+		end)
+		Ext.RegisterUITypeCall(Data.UIType.trade, "cancel", function(ui, call)
+			if lastCharacterOutsideTrade ~= "" then
+				local currentCharacter = GetClientCharacter()
+				if currentCharacter ~= nil then
+					--print(currentCharacter.UUID, "=>", lastCharacterOutsideTrade)
+					currentCharacter.UUID = lastCharacterOutsideTrade
+				end
+			end
+		end)
+		--Ext.RegisterUINameCall("charSel", OnCharacterSelected)
+		--Ext.RegisterUINameCall("selectCharacter", OnCharacterSelected)
+	end)
+
+	Ext.RegisterListener("UIObjectCreated", function(ui)
+		if ui:GetTypeId() == Data.UIType.trade then
+			local currentCharacter = GetClientCharacter()
+			if currentCharacter ~= nil then
+				lastCharacterOutsideTrade = currentCharacter.UUID
 			end
 		end
 	end)

d015344815ebf6262cc41c7ec5cf692ff0f89279
LaughingLeader <laughingleader@gmail.com>
2020-10-03 20:44:08 -0500

    Added a wrapper around osiris listeners that only run when the game is running

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Main.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Main.lua
@@ -61,6 +61,20 @@ function RemoveSkillListener(skill, callback)
 	end
 end
 
+--- Registers a function that is called when certain Osiris functions are called, but only when a game level is loaded and the gamestate is running.
+--- Supports events, built-in queries, DBs, PROCs, QRYs (user queries).
+--- @param name string Osiris function/database name
+--- @param arity number Number of columns for DBs or the number of parameters (both IN and OUT) for functions
+--- @param event string Event type ('before' - triggered before Osiris call; 'after' - after Osiris call; 'beforeDelete'/'afterDelete' - before/after delete from DB)
+--- @param handler function Lua function to run when the event fires
+function RegisterProtectedOsirisListener(name, arity, event, handler)
+	Ext.RegisterOsirisListener(name, arity, event, function(...)
+		if Ext.GetGameState() == "Running" and SharedData.RegionData.LevelType == LEVELTYPE.GAME then
+			handler(...)
+		end
+	end)
+end
+
 Ext.RegisterListener("SessionLoading", function()
 	for i,status in pairs(Ext.GetStatEntries("StatusData")) do
 		local statusType = Ext.StatGetAttribute(status, "StatusType")

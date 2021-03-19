c59427c6b994cb90f2131f7b682bf6ed4e11cbc4
LaughingLeader <laughingleader@gmail.com>
2021-03-19 16:23:29 -0500

    Added an easy way to unregister input listeners

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/InputManager.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/InputManager.lua
@@ -9,6 +9,15 @@ function Input.RegisterListener(callback)
 	RegisterListener("InputEvent", callback)
 end
 
+---@param callback InputEventCallback
+function Input.RemoveListener(callback)
+	for i,v in pairs(Listeners.InputEvent) do
+		if v == callback then
+			table.remove(Listeners.InputEvent, i)
+		end
+	end
+end
+
 ---@param evt InputEvent
 local function OnInputEvent(evt)
 	--PrintLog("[InputEvent] EventId(%s)\n  InputDeviceId(%s)\n  InputPlayerIndex(%s)\n  Press(%s)\n  Release(%s)\n  ValueChange(%s)\n  Hold(%s)\n  Repeat(%s)\n  AcceleratedRepeat(%s)", evt.EventId, evt.InputDeviceId, evt.InputPlayerIndex, evt.Press, evt.Release, evt.ValueChange, evt.Hold, evt.Repeat, evt.AcceleratedRepeat)

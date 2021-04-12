bed3f6a86de3346ad331ba41b60e04d51de41a5b
LaughingLeader <laughingleader@gmail.com>
2021-04-11 23:05:23 -0500

    Update TooltipHandler.lua

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/UI/TooltipHandler.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/UI/TooltipHandler.lua
@@ -719,6 +719,7 @@ local debugTooltipCalls = {
 
 ---@param item EclItem
 ---@param rune StatEntryObject
+---@param slot integer
 ---@param tooltip TooltipData
 local function OnRuneTooltip(item, rune, slot, tooltip)
 	if Vars.DebugMode then

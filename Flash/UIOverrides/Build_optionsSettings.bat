e2c3294aef52ef3070cdbdacf884bb907c53fc73
LaughingLeader <laughingleader@gmail.com>
2021-04-22 13:07:05 -0500

    Added batch files for replacing UI scripts via JPEXS

---
+++ Flash/UIOverrides/Build_optionsSettings.bat
@@ -0,0 +1,6 @@
+set JPEXS="D:\Modding\DOS2DE\UI_Modding\jpexs_ffdec_11.2.0_nightly1722\ffdec.bat"
+set SOURCE="D:\Modding\DOS2DE_Extracted\Public\Game\GUI\optionsSettings.swf"
+set TARGET="G:\Divinity Original Sin 2\DefEd\Data\Public\LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9\GUI\optionsSettings.swf"
+set SCRIPT_FOLDER=%~dp0
+
+%JPEXS% -replace %SOURCE% %TARGET% optionsSettings_fla.MainTimeline "%SCRIPT_FOLDER%optionsSettings\optionsSettings_fla\MainTimeline.as" optionsSettings_fla.overview_mc_1 "%SCRIPT_FOLDER%optionsSettings\optionsSettings_fla\overview_mc_1.as" Menu_button "%SCRIPT_FOLDER%optionsSettings\Menu_button.as" Selector "%SCRIPT_FOLDER%optionsSettings\Selector.as" SliderComp "%SCRIPT_FOLDER%optionsSettings\SliderComp.as" Label "%SCRIPT_FOLDER%optionsSettings\Label.as" Checkbox "%SCRIPT_FOLDER%optionsSettings\Checkbox.as" DropDown "%SCRIPT_FOLDER%optionsSettings\DropDown.as"

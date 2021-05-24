6ea5717d810916f683a850dce3831aa6c13680d3
LaughingLeader <laughingleader@gmail.com>
2021-05-24 18:52:08 -0500

    Setting up custom stats for the new icon capabilities

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/GMCustomStatTooltipFix.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/GMCustomStatTooltipFix.lua
@@ -12,11 +12,14 @@ if Ext.IsServer() then
 			if character then
 				if (character.IsPossessed or (not character.IsGameMaster and character.IsPlayer)) and character.UserID > -1 then
 					if canFix then
-						local stat = Ext.GetCustomStatByName(data.DisplayName)
+						local stat = Ext.GetCustomStatByName(data.StatId)
 						if stat then
 							data.DisplayName = stat.Name
 							data.ID = stat.Id
 							data.Description = stat.Description
+						else
+							data.DisplayName = data.StatId
+							data.Description = ""
 						end
 					end
 					Ext.PostMessageToClient(character.MyGuid, "LeaderLib_CreateCustomStatTooltip", Ext.JsonStringify(data))
@@ -79,6 +82,7 @@ else
 				Height = height,
 				Alignment = alignment,
 				DisplayName = statName or "",
+				StatId = statName or "",
 				Value = statValue
 			})
 			CustomStatTooltipFixer.Requesting = true
@@ -155,30 +159,44 @@ else
 			local data = Common.JsonParse(payload)
 			if data then
 				local statDouble = data.Stat
+				local iconid = Game.Tooltip.GetCustomStatIconId(data.ID)
+
+				if string.find(data.DisplayName, "_", 1, true) then
+					data.DisplayName = GameHelpers.Tooltip.ReplacePlaceholders(GameHelpers.GetStringKeyText(data.DisplayName, data.DisplayName))
+					print(data.DisplayName)
+				end
+				if string.find(data.Description, "_", 1, true) then
+					data.Description = GameHelpers.Tooltip.ReplacePlaceholders(GameHelpers.GetStringKeyText(data.Description, data.Description))
+				end
+
 				Game.Tooltip.SaveCustomStat({
 					HandleDouble = data.Stat,
 					UUID = data.ID,
+					StatId = data.StatId,
 					DisplayName = data.DisplayName,
 					Description = data.Description,
-					Value = data.Value
+					Value = data.Value,
+					IconId = iconid,
+					IconAtlasKey = "Talent_BackStabber"--"Ability_Summoning"
 				})
 				local character = Ext.GetCharacter(data.Character)
 				local ui = Ext.GetUIByType(Data.UIType.tooltip)
 				if ui then
-					local main = ui:GetRoot()
-					if main and main.tooltip_array then
-						main.tooltip_array[0] = Game.Tooltip.TooltipItemTypes.StatName
-						main.tooltip_array[1] = data.DisplayName or ""
-						main.tooltip_array[2] = Game.Tooltip.TooltipItemTypes.StatsDescription
-						main.tooltip_array[3] = data.Description or ""
+					local this = ui:GetRoot()
+					if this and this.tooltip_array then
+						-- this.tooltip_array[0] = Game.Tooltip.TooltipItemTypes.StatName
+						-- this.tooltip_array[1] = data.DisplayName or ""
+						-- this.tooltip_array[2] = Game.Tooltip.TooltipItemTypes.StatsDescription
+						-- this.tooltip_array[3] = data.Description or ""
 						--AbilityDescription = {{"AbilityId", "number"}, {"Description", "string"}, {"Description2", "string"}, {"CurrentLevelEffect", "string"}, {"NextLevelEffect", "string"}},
-						--main.tooltip_array[0] = Game.Tooltip.TooltipItemTypes.AbilityTitle
-						-- main.tooltip_array[2] = Game.Tooltip.TooltipItemTypes.AbilityDescription
-						-- main.tooltip_array[3] = 34
-						-- main.tooltip_array[4] = data.Description or ""
-						-- main.tooltip_array[5] = ""
-						-- main.tooltip_array[6] = ""
-						-- main.tooltip_array[7] = ""
+						this.tooltip_array[0] = Game.Tooltip.TooltipItemTypes.AbilityTitle
+						this.tooltip_array[1] = data.DisplayName or ""
+						this.tooltip_array[2] = Game.Tooltip.TooltipItemTypes.AbilityDescription
+						this.tooltip_array[3] = iconid
+						this.tooltip_array[4] = data.Description or ""
+						this.tooltip_array[5] = ""
+						this.tooltip_array[6] = ""
+						this.tooltip_array[7] = ""
 	
 						--ui:ExternalInterfaceCall("showTooltip", "", data.X, data.Y,data.Width,data.Height,"right",true)
 						--ui:ExternalInterfaceCall("clearAnchor")
@@ -186,7 +204,7 @@ else
 						--TODO Figure out how to move the tooltip UI to the proper x/y position.
 						--It's like the contextMenu in that its position isn't 1:1 a screen position
 
-						local tf = main.formatTooltip
+						local tf = this.formatTooltip
 						if tf then
 							lastTooltipX = tf.x
 							lastTooltipY = tf.y
@@ -202,23 +220,32 @@ else
 						ui:Invoke("showFormattedTooltipAfterPos", false)
 
 						CustomStatTooltipFixer.Visible = true
-						local tf = main.formatTooltip
+						local tf = this.formatTooltip or this.tf
 						if tf then
-							-- lastTooltipX = tf.x
-							-- lastTooltipY = tf.y
 							tf.x = 50
 							tf.y = 90
-							--local x,y = UIExtensions.GetMousePosition()
-							-- tf.x = x + 50
-							-- tf.y = y
-							-- for i=0,#tf.tooltip_mc.list.content_array do
-							-- 	local entry = tf.tooltip_mc.list.content_array[i]
-							-- 	if entry and entry.initImage then
-							-- 		entry.initImage("iggy_tt_ability_1", 20)
-							-- 	end
-							-- end
 						end
 						ui:ExternalInterfaceCall("keepUIinScreen", false)
+						-- UIExtensions.StartTimer("tooltipText", 10, function()
+						-- 	--ui:SetCustomIcon("i0", "Ability_Summoning", 128, 128)
+						-- 	local tooltip_mc = this.tf.tooltip_mc
+						-- 	if tooltip_mc then
+						-- 		print("AbilityId", tooltip_mc.AbilityId)
+						-- 		print("areInaccessibleObjectsUnderPoint", tooltip_mc.areInaccessibleObjectsUnderPoint)
+						-- 		for i=0,#tooltip_mc.list.content_array-1 do
+						-- 			local entry = tooltip_mc.list.content_array[i]
+						-- 			print(i, entry and entry.name or "nil?")
+						-- 			if entry then
+						-- 				fprint(LOGLEVEL.TRACE, "groupID(%s) orderId(%s) iconId(%s)", entry.groupID or "nil", entry.orderId or "nil", entry.iconId or "nil")
+						-- 				if entry.container_mc then
+						-- 					---@type FlashMovieClip
+						-- 					local container_mc = entry.container_mc
+						-- 					fprint(LOGLEVEL.TRACE, "tt_statsIcon? numChildren(%s) child(%s)", container_mc.numChildren or "nil", container_mc.getChildAt(0) or "nil")
+						-- 				end
+						-- 			end
+						-- 		end
+						-- 	end
+						-- end)
 					end
 				end
 			end

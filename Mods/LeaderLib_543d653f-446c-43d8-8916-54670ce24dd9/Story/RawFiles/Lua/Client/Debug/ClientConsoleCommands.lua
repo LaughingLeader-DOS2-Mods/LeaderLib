942e362de946c58846df2de1334556f81d7589cf
LaughingLeader <laughingleader@gmail.com>
2021-06-04 21:30:47 -0500

    Fixes for GM toggling to make sure the level buttons are always visible

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/Debug/ClientConsoleCommands.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/Debug/ClientConsoleCommands.lua
@@ -2,14 +2,37 @@ Ext.RegisterConsoleCommand("luareset", function(cmd, delay)
 	Ext.PostMessageToServer("LeaderLib_Client_RequestLuaReset", delay or "")
 end)
 
-Input.RegisterListener("ToggleCraft", function(event, pressed)
-	print("ToggleCraft", pressed, "ToggleInfo", Input.IsPressed("ToggleInfo"))
+Ext.RegisterConsoleCommand("abilityTest", function(cmd, enabled)
+	if enabled == "false" then
+		AbilityManager.DisableAbility("all", ModuleUUID)
+	else
+		AbilityManager.EnableAbility("all", ModuleUUID)
+	end
+end)
+
+local pointsWarn = {}
+
+Input.RegisterListener("ToggleCraft", function(event, pressed, id, keys, controllerEnabled)
 	if Input.IsPressed("ToggleInfo") then
 		local this = Ext.GetUIByType(Data.UIType.characterSheet):GetRoot()
 		Ext.Print("Toggling GM mode in character sheet: ", not this.isGameMasterChar)
 		if this.isGameMasterChar then
 			this.setGameMasterMode(false, false, false)
+			this.stats_mc.setVisibilityStatButtons(false)
+			this.stats_mc.setVisibilityAbilityButtons(true, false)
+			this.stats_mc.setVisibilityAbilityButtons(false, false)
+			this.stats_mc.setVisibilityTalentButtons(false)
+			Ext.PostMessageToServer("LeaderLib_RefreshCharacterSheet", Client.Character.UUID)
+
+			for i,v in pairs(pointsWarn) do
+				this.stats_mc.INTSetWarnAndPoints(i,v)
+			end
+			pointsWarn = {}
 		else
+			for i=0,#this.stats_mc.pointsWarn-1 do
+				pointsWarn[i] = this.stats_mc.pointsWarn[i].avPoints
+			end
+
 			this.setGameMasterMode(true, true, false)
 		end
 	end

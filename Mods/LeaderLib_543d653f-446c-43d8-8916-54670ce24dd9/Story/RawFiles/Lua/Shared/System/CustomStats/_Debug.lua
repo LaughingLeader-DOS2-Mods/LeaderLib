d40d41c4599045503492f9316c205924bf65bc24
LaughingLeader <laughingleader@gmail.com>
2021-06-13 13:00:26 -0500

    Debug table hiding from EmmyLua

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/System/CustomStats/_Debug.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/System/CustomStats/_Debug.lua
@@ -1,19 +1,23 @@
 if Ext.Version() < 55 then
 	-- Intermediate update workaround - Still working on this system
 	CustomStatSystem = {}
-	function CustomStatSystem:OnToggleCharacterPane() end
-	function CustomStatSystem:OnRequestTooltip() end
-	function CustomStatSystem:UpdateStatTooltipArray() end
-	function CustomStatSystem:AddAvailablePoints() end
-	function CustomStatSystem:SyncData() end
-	function CustomStatSystem:GetStatByDouble() end
-	function CustomStatSystem:OnTooltip() end
-	function CustomStatSystem:RegisterAvailablePointsChangedListener() end
-	function CustomStatSystem:RegisterStatValueChangedListener() end
-	function CustomStatSystem:RegisterCanAddPointsHandler() end
-	function CustomStatSystem:RegisterCanRemovePointsHandler() end
 
-	CustomStatSystem.__index = function(tbl,k)
+	--Prevent EmmyLua from pointing towards here
+	---@type table
+	local placeholder = CustomStatSystem
+	function placeholder:OnToggleCharacterPane() end
+	function placeholder:OnRequestTooltip() end
+	function placeholder:UpdateStatTooltipArray() end
+	function placeholder:AddAvailablePoints() end
+	function placeholder:SyncData() end
+	function placeholder:GetStatByDouble() end
+	function placeholder:OnTooltip() end
+	function placeholder:RegisterAvailablePointsChangedListener() end
+	function placeholder:RegisterStatValueChangedListener() end
+	function placeholder:RegisterCanAddPointsHandler() end
+	function placeholder:RegisterCanRemovePointsHandler() end
+
+	placeholder.__index = function(tbl,k)
 		return function() end
 	end
 end

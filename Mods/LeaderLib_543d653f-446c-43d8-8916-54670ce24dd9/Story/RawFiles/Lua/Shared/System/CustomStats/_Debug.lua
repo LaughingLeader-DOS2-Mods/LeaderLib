5266316a01c0340afd0e5abb96b57bf3962c7d65
LaughingLeader <laughingleader@gmail.com>
2021-06-13 08:30:37 -0500

    Disable the custom stats feature flags until v55 is public
    These flags, without the character sheet overrides, will make the tags tab get replaced with the custom stats one, so this needs to be removed for now.

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/System/CustomStats/_Debug.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/System/CustomStats/_Debug.lua
@@ -1,7 +1,6 @@
 if Ext.Version() < 55 then
 	-- Intermediate update workaround - Still working on this system
 	CustomStatSystem = {}
-	CustomStatSystem.__index = CustomStatSystem
 	function CustomStatSystem:OnToggleCharacterPane() end
 	function CustomStatSystem:OnRequestTooltip() end
 	function CustomStatSystem:UpdateStatTooltipArray() end
@@ -9,4 +8,12 @@ if Ext.Version() < 55 then
 	function CustomStatSystem:SyncData() end
 	function CustomStatSystem:GetStatByDouble() end
 	function CustomStatSystem:OnTooltip() end
+	function CustomStatSystem:RegisterAvailablePointsChangedListener() end
+	function CustomStatSystem:RegisterStatValueChangedListener() end
+	function CustomStatSystem:RegisterCanAddPointsHandler() end
+	function CustomStatSystem:RegisterCanRemovePointsHandler() end
+
+	CustomStatSystem.__index = function(tbl,k)
+		return function() end
+	end
 end

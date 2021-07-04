ad4fdf8273fd871d8eede273f1931f0b4fcf0655
LaughingLeader <laughingleader@gmail.com>
2021-07-04 05:39:03 -0500

    Renaming AbilityAPI script to AbilityManager

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapShared.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapShared.lua
@@ -28,7 +28,7 @@ Ext.Require("Shared/SharedDebug.lua")
 Ext.Require("Shared/Stats/CustomSkillProperties.lua")
 Ext.Require("Shared/Stats/StatOverrides.lua")
 Ext.Require("Shared/System/FeaturesHandler.lua")
-Ext.Require("Shared/System/AbilityAPI.lua")
+Ext.Require("Shared/System/AbilityManager.lua")
 
 if Ext.Version() >= 55 then
 Ext.Require("Shared/System/CustomStats/CustomStatSystem.lua")

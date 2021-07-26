612bb5dcb6749415e80f4d3cbe592976495a5097
LaughingLeader <laughingleader@gmail.com>
2021-07-26 13:21:39 -0500

    Set Game.Tooltip.NextRequest

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/UI/SheetManager/TooltipCreator.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/UI/SheetManager/TooltipCreator.lua
@@ -8,6 +8,11 @@ local function CreateTooltip(tooltipType, requestedUI, call, id)
 		local this = ui:GetRoot()
 		local data = SheetManager:GetStatByGeneratedID(id, tooltipType)
 		if this and this.tooltip_array and data then
+			local request = {
+				Type = tooltipType,
+				Character = Client:GetCharacter()
+			}
+
 			local resolved = false
 			if tooltipType == "Ability" then
 				this.tooltip_array[0] = Game.Tooltip.TooltipItemTypes.StatName
@@ -19,6 +24,8 @@ local function CreateTooltip(tooltipType, requestedUI, call, id)
 				this.tooltip_array[6] = ""
 				this.tooltip_array[7] = ""
 
+				request.Ability = data.ID
+
 				if data.Icon then
 					Game.Tooltip.PrepareIcon(ui, string.format("tt_ability_%i", data.GeneratedID), data.Icon, data.IconWidth or 128, data.IconHeight or 128)
 				end
@@ -26,12 +33,16 @@ local function CreateTooltip(tooltipType, requestedUI, call, id)
 			elseif tooltipType == "Talent" then
 				this.tooltip_array[0] = Game.Tooltip.TooltipItemTypes.StatName
 				this.tooltip_array[1] = data:GetDisplayName()
+				--TalentDescription = {{"TalentId", "number"}, {"Description", "string"}, {"Requirement", "string"}, {"IncompatibleWith", "string"}, {"Selectable", "boolean"}, {"Unknown", "boolean"}},
 				this.tooltip_array[2] = Game.Tooltip.TooltipItemTypes.TalentDescription
 				this.tooltip_array[3] = data.GeneratedID
 				this.tooltip_array[4] = data:GetDescription()
 				this.tooltip_array[5] = ""
 				this.tooltip_array[6] = ""
-				this.tooltip_array[7] = ""
+				this.tooltip_array[7] = true
+				this.tooltip_array[8] = true
+
+				request.Talent = data.ID
 
 				if data.Icon then
 					Game.Tooltip.PrepareIcon(ui, string.format("tt_talent_%i", data.GeneratedID), data.Icon, data.IconWidth or 128, data.IconHeight or 128)
@@ -42,6 +53,8 @@ local function CreateTooltip(tooltipType, requestedUI, call, id)
 				this.tooltip_array[1] = data:GetDisplayName()
 				this.tooltip_array[2] = Game.Tooltip.TooltipItemTypes.StatsDescription
 				this.tooltip_array[3] = data:GetDescription()
+
+				request.Stat = data.ID
 				resolved = true
 			end
 
@@ -50,7 +63,14 @@ local function CreateTooltip(tooltipType, requestedUI, call, id)
 				this.tooltip_array[1] = data:GetDisplayName()
 				this.tooltip_array[2] = Game.Tooltip.TooltipItemTypes.StatsDescription
 				this.tooltip_array[3] = data:GetDescription()
+				-- request.Text = this.tooltip_array[3]
+				-- request.Type = "Generic"
+				-- request.CallingUI = ui:GetTypeId()
 			end
+
+			Game.Tooltip.TooltipHooks.NextRequest = request
+			Game.Tooltip.TooltipHooks.Last.Event = call
+			Game.Tooltip.TooltipHooks.Last.UIType = requestedUI:GetTypeId()
 
 			isVisible = true
 
@@ -59,6 +79,7 @@ local function CreateTooltip(tooltipType, requestedUI, call, id)
 				lastTooltipX = tf.x
 				lastTooltipY = tf.y
 			end
+
 			ui:ExternalInterfaceCall("setAnchor","left","mouse","left")
 			ui:Invoke("addFormattedTooltip",0,0,true)
 			local tf = this.formatTooltip or this.tf

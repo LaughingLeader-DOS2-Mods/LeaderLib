6ffdefed16d94d992f39fc0256bb8603cc90e459
LaughingLeader <laughingleader@gmail.com>
2021-08-27 13:01:33 -0500

    Added UIWrapper functions for registering callbacks

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/Classes/UIWrapper.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Client/Classes/UIWrapper.lua
@@ -61,6 +61,11 @@ function UIWrapper:CreateFromType(id, params)
 		end
 	end
 	SetMeta(this)
+	local ui = Ext.GetUIByType(id) or Ext.GetUIByType(this.ControllerID)
+	if ui then
+		ui:CaptureExternalInterfaceCalls()
+		ui:CaptureInvokes()
+	end
 	return this
 end
 
@@ -82,9 +87,65 @@ function UIWrapper:CreateFromPath(path, params)
 	if ui then
 		this.ID = ui:GetTypeId()
 		this.Name = Data.UITypeToName[this.ID]
+		ui:CaptureExternalInterfaceCalls()
+		ui:CaptureInvokes()
 	end
 	SetMeta(this)
 	return this
+end
+
+---@alias UIWrapperEventContextType string|'"Keyboard"'|'"Controller"'|'"All"'
+---@alias UIWrapperCallbackHandler fun(self:LeaderLibUIWrapper, ui:UIObject, event:string, vararg any):void
+
+---@param event string The method name.
+---@param callback UIWrapperCallbackHandler
+---@param eventType UICallbackEventType
+---@param uiContext UIWrapperEventContextType
+function UIWrapper:RegisterInvokeListener(event, callback, eventType, uiContext)
+	if self.ID ~= -1 and uiContext ~= "Controller" then
+		Ext.RegisterUITypeInvokeListener(self.ID, event, function(...)
+			print(self, event, ...)
+			local b,err = xpcall(callback, debug.traceback, self, ...)
+			if not b then
+				fprint(LOGLEVEL.ERROR, "[UIWrapper(%s):InvokeListener] Error:%s", self.ID, err)
+				error(err, 2)
+			end
+		end, eventType)
+	end
+	if self.ControllerID ~= -1 and (uiContext == "Controller" or uiContext == "All") then
+		Ext.RegisterUITypeInvokeListener(self.ControllerID, event, function(...)
+			local b,err = xpcall(callback, debug.traceback, self, ...)
+			if not b then
+				fprint(LOGLEVEL.ERROR, "[UIWrapper(%s):InvokeListener] Error:%s", self.ControllerID, err)
+				error(err, 2)
+			end
+		end, eventType)
+	end
+end
+
+---@param event string The ExternalInterface.call name.
+---@param callback UIWrapperCallbackHandler
+---@param eventType UICallbackEventType
+---@param uiContext UIWrapperEventContextType
+function UIWrapper:RegisterCallListener(event, callback, eventType, uiContext)
+	if self.ID ~= -1 and uiContext ~= "Controller" then
+		Ext.RegisterUITypeCall(self.ID, event, function(...)
+			local b,err = xpcall(callback, debug.traceback, self, ...)
+			if not b then
+				fprint(LOGLEVEL.ERROR, "[UIWrapper(%s):CallListener] Error:%s", self.ID, err)
+				error(err, 2)
+			end
+		end, eventType)
+	end
+	if self.ControllerID ~= -1 and (uiContext == "Controller" or uiContext == "All") then
+		Ext.RegisterUITypeCall(self.ControllerID, event, function(...)
+			local b,err = xpcall(callback, debug.traceback, self, ...)
+			if not b then
+				fprint(LOGLEVEL.ERROR, "[UIWrapper(%s):CallListener] Error:%s", self.ControllerID, err)
+				error(err, 2)
+			end
+		end, eventType)
+	end
 end
 
 ---@return UIObject

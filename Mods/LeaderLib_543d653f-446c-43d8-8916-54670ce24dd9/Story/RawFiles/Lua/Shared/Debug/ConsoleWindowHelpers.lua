60596727aea5524f49bff072d7e8dad955e17368
LaughingLeader <laughingleader@gmail.com>
2021-10-09 12:07:49 -0500

    Made some console vars debug mode only

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Debug/ConsoleWindowHelpers.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Debug/ConsoleWindowHelpers.lua
@@ -49,80 +49,6 @@ if not isClient then
 	})
 	AddConsoleVariable("me", me)
 
-	local character = {}
-	function character.GetAll(props, includeDefault, exportToFileName)
-		local data = Ext.GetAllCharacters()
-		setmetatable(data, {
-			__tostring = function()
-				local text = {}
-				local printProps = props and type(props) == "table"
-				for _,v in pairs(data) do
-					local c = Ext.GetCharacter(v)
-					local entry = {}
-					if printProps then
-						for _,propertyName in pairs(props) do
-							if propertyName == "Tags" then
-								local tags = c:GetTags()
-								table.sort(tags)
-								entry.Tags = tags
-							else
-								local value = c[propertyName] or c.RootTemplate[propertyName] or c.Stats[propertyName] or nil
-								if type(value) == "function" then
-									local b,result = xpcall(value, debug.traceback, c)
-									if not b then
-										Ext.PrintError(result)
-									else
-										if string.find(propertyName, "Statuses") then
-											propertyName = "Statuses"
-										end
-										entry[propertyName] = result
-									end
-								elseif value then
-									entry[propertyName] = tostring(value)
-								end
-							end
-						end
-					end
-					if not printProps or includeDefault then
-						entry.DisplayName = GameHelpers.Character.GetDisplayName(c)
-						entry.NetID = c.NetID
-						entry.StatsId = c.Stats.Name
-						entry.Name = c.RootTemplate.Name
-						entry.RootTemplate = c.RootTemplate.TemplateName
-						if c.OffStage then
-							entry.OffStage = true
-						end
-					end
-					text[v] = entry
-				end
-				return Ext.JsonStringify(text)
-			end
-		})
-		if exportToFileName then
-			Ext.SaveFile(string.format("ConsoleDebug/%s.json", exportToFileName), tostring(data))
-		end
-		return data
-	end
-	setmetatable(character, {
-		__call = function(tbl, uuid)
-			local t = type(uuid)
-			if t == "string" then
-				if ObjectExists(uuid) == 0 then
-					error(string.format("UUID '%s' does not exist!", uuid))
-				end
-				return Ext.GetCharacter(uuid)
-			elseif t == "number" then
-				return Ext.GetCharacter(uuid)
-			end
-			return nil
-		end,
-		__index = function(tbl,k)
-			Ext.PrintWarning("Call character() with a uuid first.")
-			return character
-		end
-	})
-	AddConsoleVariable("character", character)
-
 	local party = {}
 	function party.ApplyStatus(entries, status, duration, force)
 		if entries == party or entries == nil then
@@ -240,6 +166,83 @@ if not isClient then
 		end
 	})
 	AddConsoleVariable("party", party)
+
+
+	if Vars.DebugMode then
+		local character = {}
+		function character.GetAll(props, includeDefault, exportToFileName)
+			local data = Ext.GetAllCharacters()
+			setmetatable(data, {
+				__tostring = function()
+					local text = {}
+					local printProps = props and type(props) == "table"
+					for _,v in pairs(data) do
+						local c = Ext.GetCharacter(v)
+						local entry = {}
+						if printProps then
+							for _,propertyName in pairs(props) do
+								if propertyName == "Tags" then
+									local tags = c:GetTags()
+									table.sort(tags)
+									entry.Tags = tags
+								else
+									local value = c[propertyName] or c.RootTemplate[propertyName] or c.Stats[propertyName] or nil
+									if type(value) == "function" then
+										local b,result = xpcall(value, debug.traceback, c)
+										if not b then
+											Ext.PrintError(result)
+										else
+											if string.find(propertyName, "Statuses") then
+												propertyName = "Statuses"
+											end
+											entry[propertyName] = result
+										end
+									elseif value then
+										entry[propertyName] = tostring(value)
+									end
+								end
+							end
+						end
+						if not printProps or includeDefault then
+							entry.DisplayName = GameHelpers.Character.GetDisplayName(c)
+							entry.NetID = c.NetID
+							entry.StatsId = c.Stats.Name
+							entry.Name = c.RootTemplate.Name
+							entry.RootTemplate = c.RootTemplate.TemplateName
+							if c.OffStage then
+								entry.OffStage = true
+							end
+						end
+						text[v] = entry
+					end
+					return Ext.JsonStringify(text)
+				end
+			})
+			if exportToFileName then
+				Ext.SaveFile(string.format("ConsoleDebug/%s.json", exportToFileName), tostring(data))
+			end
+			return data
+		end
+		setmetatable(character, {
+			__call = function(tbl, uuid)
+				local t = type(uuid)
+				if t == "string" then
+					if ObjectExists(uuid) == 0 then
+						error(string.format("UUID '%s' does not exist!", uuid))
+					end
+					return Ext.GetCharacter(uuid)
+				elseif t == "number" then
+					return Ext.GetCharacter(uuid)
+				end
+				return nil
+			end,
+			__index = function(tbl,k)
+				Ext.PrintWarning("Call character() with a uuid first.")
+				return character
+			end
+		})
+		AddConsoleVariable("character", character)
+	end
 else
 	local me = {}
 	setmetatable(me, {
@@ -273,32 +276,34 @@ else
 	})
 	AddConsoleVariable("me", me)
 
-	local sheet = {}
-	setmetatable(sheet, {
-		__call = function()
-			return Ext.GetUIByType(Data.UIType.characterSheet):GetRoot()
-		end,
-		__index = function(tbl,k)
-			local ui = Ext.GetUIByType(Data.UIType.characterSheet):GetRoot()
-			local v = ui[k]
-			if type(v) == "function" then
-				return function(...)
-					local b,result = pcall(v, ui, ...)
-					return result
+	if Vars.DebugMode then
+		local sheet = {}
+		setmetatable(sheet, {
+			__call = function()
+				return Ext.GetUIByType(Data.UIType.characterSheet):GetRoot()
+			end,
+			__index = function(tbl,k)
+				local ui = Ext.GetUIByType(Data.UIType.characterSheet):GetRoot()
+				local v = ui[k]
+				if type(v) == "function" then
+					return function(...)
+						local b,result = pcall(v, ui, ...)
+						return result
+					end
+				else
+					return v
 				end
-			else
-				return v
+			end,
+			__newindex = function(tbl,k,v)
+				local ui = Ext.GetUIByType(Data.UIType.characterSheet):GetRoot()
+				if ui then
+					ui[k] = v
+				end
+			end,
+			__tostring = function()
+				return tostring(Data.UIType.characterSheet)
 			end
-		end,
-		__newindex = function(tbl,k,v)
-			local ui = Ext.GetUIByType(Data.UIType.characterSheet):GetRoot()
-			if ui then
-				ui[k] = v
-			end
-		end,
-		__tostring = function()
-			return tostring(Data.UIType.characterSheet)
-		end
-	})
-	AddConsoleVariable("sheet", sheet)
+		})
+		AddConsoleVariable("sheet", sheet)
+	end
 end

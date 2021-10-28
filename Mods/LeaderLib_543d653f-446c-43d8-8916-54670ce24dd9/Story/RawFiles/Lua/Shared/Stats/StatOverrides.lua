e79188647edee1db703af705b6ead7331254a335
LaughingLeader <laughingleader@gmail.com>
2021-10-28 06:33:40 -0500

    Separated force skills, testing stat file path overriding

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Stats/StatOverrides.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Stats/StatOverrides.lua
@@ -73,6 +73,7 @@ end
 
 -- Adds more alignment entities
 Ext.AddPathOverride("Mods/DivinityOrigins_1301db3d-1f54-4e98-9be5-5094030916e4/Story/Alignments/Alignment.lsx", "Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Overrides/OriginsAlignments.lsx")
+Ext.AddPathOverride("Public/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Stats/Generated/Data/LeaderLib_Skills_SafeForce.txt", "Public/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Stats/Overrides/LeaderLib_Skills_SafeForce.txt")
 
 ---Modifies a stat if it differs from the desired value.
 ---@param stat StatEntryCharacter
@@ -120,7 +121,7 @@ local function OverrideForce(syncMode, skills)
 			end
 		end
 	end
-	if Vars.DebugMode then
+	--[[ if Vars.DebugMode then
 		for _,v in pairs(skills) do
 			---@type StatProperty[]
 			local props = nil
@@ -139,10 +140,11 @@ local function OverrideForce(syncMode, skills)
 					if prop.Type == "Force" then
 						local dist = prop.Distance or 1.0
 						hasForce = true
-						props[i] = {
+						---@type StatPropertyExtender
+						local extProp = {
 							Type = "Extender",
 							Action = "SafeForce",
-							Arg1 = 1.0,
+							Arg1 = 1,
 							Arg2 =  Ext.Round(dist * 6),
 							Arg3 =  "",
 							Arg4 =  -1,
@@ -152,6 +154,7 @@ local function OverrideForce(syncMode, skills)
 								"AoE",
 							}
 						}
+						props[i] = extProp
 					end
 				end
 				if hasForce then
@@ -164,7 +167,7 @@ local function OverrideForce(syncMode, skills)
 				end
 			end
 		end
-	end
+	end ]]
 end
 
 local function AdjustAP(stat, settings)
@@ -187,10 +190,9 @@ local function AdjustAP(stat, settings)
 	return changedStat
 end
 
----@param isServer boolean
 ---@param data LeaderLibGameSettings
----@param forceSync boolean|nil
-local function OverrideStats(data)
+---@param statsLoadedState boolean
+local function OverrideStats(data, statsLoadedState)
 	fprint(LOGLEVEL.DEFAULT, "[LeaderLib:SyncStatOverrides:%s] Syncing stat overrides from GameSettings.", isClient and "CLIENT" or "SERVER")
 	if data == nil then
 		data = GameSettingsManager.Load()
@@ -217,8 +219,10 @@ local function OverrideStats(data)
 					originalSkillTiers[id] = tier
 					total = total + 1
 					stat.Tier = "Starter"
-					if not isClient then
+					if not isClient and not statsLoadedState then
 						Ext.SyncStat(id, false)
+					else
+						Ext.StatSetAttribute(id, "Tier", "Starter")
 					end
 				end
 			else
@@ -230,7 +234,7 @@ local function OverrideStats(data)
 				local changed = false
 				if memorizationReq ~= nil then
 					for i,v in pairs(memorizationReq) do
-						if Data.AbilityEnum[v.Requirement] ~= nil and v.Param > 1 then
+						if Data.AbilityEnum[v.Requirement] ~= nil and type(v.Param) == "number" and v.Param > 1 then
 							v.Param = 1
 							changed = true
 						end
@@ -238,8 +242,10 @@ local function OverrideStats(data)
 				end
 				if changed then
 					stat.MemorizationRequirements = memorizationReq
-					if not isClient then
+					if not isClient and not statsLoadedState then
 						Ext.SyncStat(id, false)
+					else
+						Ext.StatSetAttribute(id, "MemorizationRequirements", memorizationReq)
 					end
 				end
 			end
@@ -254,11 +260,18 @@ local function OverrideStats(data)
 		local settings = data.Settings.APSettings.Player
 		for id,b in pairs(player_stats) do
 			if b == true or (type(b) == "string" and Ext.IsModLoaded(b)) then
+				---@type StatEntryCharacter
 				local stat = Ext.GetStat(id)
 				if stat then
 					local changedStat = AdjustAP(stat, settings)
-					if not isClient and changedStat then
-						Ext.SyncStat(id, false)
+					if changedStat then
+						if not isClient and not statsLoadedState then
+							Ext.SyncStat(id, false)
+						else
+							Ext.StatSetAttribute(id, "APStart", stat.APStart)
+							Ext.StatSetAttribute(id, "APRecovery", stat.APRecovery)
+							Ext.StatSetAttribute(id, "APRecovery", stat.APMaximum)
+						end
 					end
 				end
 			end
@@ -287,17 +300,26 @@ local function OverrideStats(data)
 			if not skip then
 				local stat = Ext.GetStat(id)
 				local changedStat = AdjustAP(stat, settings)
-				if not isClient and changedStat then
-					Ext.SyncStat(id, false)
+				if changedStat then
+					if not isClient and not statsLoadedState then
+						Ext.SyncStat(id, false)
+					else
+						Ext.StatSetAttribute(id, "APStart", stat.APStart)
+						Ext.StatSetAttribute(id, "APRecovery", stat.APRecovery)
+						Ext.StatSetAttribute(id, "APRecovery", stat.APMaximum)
+					end
 				end
 			end
 		end
 	end
-	OverrideWings(not isClient)
-	OverrideForce(not isClient, skills)
+	OverrideWings(not isClient and not statsLoadedState)
+	OverrideForce(not isClient and not statsLoadedState, skills)
 end
-Ext.RegisterListener("StatsLoaded", OverrideStats)
+
+Ext.RegisterListener("StatsLoaded", function()
+	OverrideStats(nil, false)
+end)
 
 function SyncStatOverrides(data, force)
-	OverrideStats(data, force)
+	OverrideStats(data)
 end

a7a0fa3b2ed5656f8fcc9ccb8eb8f8a13966a71d
LaughingLeader <laughingleader@gmail.com>
2021-11-18 01:56:18 -0600

    Tweak GameHelpers.Combat.GetCharacters to support relation checks

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Game/Helpers/CombatHelpers.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Game/Helpers/CombatHelpers.lua
@@ -7,10 +7,12 @@ end
 
 ---@param id integer
 ---@param filter GameHelpersCombatGetCharactersFilter|GameHelpersCombatGetCharactersFilterCallback|nil Used to filter returned charaters. Allies/Enemies/Neutral are the alignment relation towards the player party. If a function is supplied instead, a character is only included if the function returns true.
----@return EsvCharacter[]
-function GameHelpers.Combat.GetCharacters(id, filter)
+---@param filterReference EsvCharacter|EsvItem For when using preset filters like "Ally", is is a reference character for relational checks.
+---@return fun():EsvCharacter
+function GameHelpers.Combat.GetCharacters(id, filter, filterReference)
 	local combat = Ext.GetCombat(id)
 	if combat then
+		local refuuid = GameHelpers.GetUUID(filterReference)
 		local objects = {}
 		for i,v in pairs(combat:GetAllTeams()) do
 			if filter then
@@ -23,16 +25,30 @@ function GameHelpers.Combat.GetCharacters(id, filter)
 						objects[#objects+1] = v.Character
 					end
 				elseif t == "string" then
-					if filter == "Player" and v.Character.IsPlayer then
-						objects[#objects+1] = v.Character
-					elseif filter == "Ally" and GameHelpers.Character.IsAllyOfParty(v.Character.MyGuid) then
-						objects[#objects+1] = v.Character
-					elseif filter == "Enemy" and GameHelpers.Character.IsEnemyOfParty(v.Character.MyGuid) then
-						objects[#objects+1] = v.Character
-					elseif filter == "Neutral" and GameHelpers.Character.IsNeutralToParty(v.Character.MyGuid) then
-						objects[#objects+1] = v.Character
-					elseif filter == "None" then
-						objects[#objects+1] = v.Character
+					if refuuid then
+						if filter == "Player" and v.Character.IsPlayer then
+							objects[#objects+1] = v.Character
+						elseif filter == "Ally" and CharacterIsAlly(refuuid, v.Character.MyGuid) == 1 then
+							objects[#objects+1] = v.Character
+						elseif filter == "Enemy" and CharacterIsEnemy(refuuid, v.Character.MyGuid) == 1 then
+							objects[#objects+1] = v.Character
+						elseif filter == "Neutral" and CharacterIsNeutral(refuuid, v.Character.MyGuid) == 1 then
+							objects[#objects+1] = v.Character
+						elseif filter == "None" then
+							objects[#objects+1] = v.Character
+						end
+					else
+						if filter == "Player" and v.Character.IsPlayer then
+							objects[#objects+1] = v.Character
+						elseif filter == "Ally" and GameHelpers.Character.IsAllyOfParty(v.Character.MyGuid) then
+							objects[#objects+1] = v.Character
+						elseif filter == "Enemy" and GameHelpers.Character.IsEnemyOfParty(v.Character.MyGuid) then
+							objects[#objects+1] = v.Character
+						elseif filter == "Neutral" and GameHelpers.Character.IsNeutralToParty(v.Character.MyGuid) then
+							objects[#objects+1] = v.Character
+						elseif filter == "None" then
+							objects[#objects+1] = v.Character
+						end
 					end
 				end
 			else

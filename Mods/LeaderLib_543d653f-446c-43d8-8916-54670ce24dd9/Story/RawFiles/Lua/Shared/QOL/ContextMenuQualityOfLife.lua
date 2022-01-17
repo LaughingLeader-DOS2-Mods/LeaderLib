0e91f8f3617e71ae2cf2ef6166cb2123abdcffed
LaughingLeader <laughingleader@gmail.com>
2022-01-16 18:04:12 -0600

    Fix NetID overlap for items/characters for context menu info copying
    Since NetID can be equal for an item and character, their type needs to be specified before trying to get the object with the appropriate function. Copying equipment info is now supported as well.

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/QOL/ContextMenuQualityOfLife.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/QOL/ContextMenuQualityOfLife.lua
@@ -19,8 +19,122 @@ if isClient then
 
 	local registeredListeners = false
 
-	local NETID_TO_UUID = {}
-	local NETID_TO_ROTATION = {}
+	local NETID_TO_UUID = {
+		Character = {},
+		Item = {}
+	}
+	local UUID_TO_ROTATION = {}
+	local sheetItemContextMenuDouble = nil
+
+	local function SaveInfoToFile(netid, hoverType)
+		local obj = nil
+		if hoverType then
+			if hoverType == "Character" then
+				obj = GameHelpers.GetCharacter(netid)
+			elseif hoverType == "Item" then
+				obj = GameHelpers.GetItem(netid)
+			end
+		else
+			obj = GameHelpers.TryGetObject(netid)
+		end
+		if obj then
+			if not hoverType then
+				if GameHelpers.Ext.ObjectIsCharacter(obj) then
+					hoverType = "Character"
+				else
+					hoverType = "Item"
+				end
+			end
+			Ext.Dump({
+				UUID = obj.MyGuid,
+				NetID = obj.NetID,
+				DisplayName = obj.DisplayName,
+				FuncNetID = netid,
+			})
+			local uuid = obj.MyGuid or NETID_TO_UUID[hoverType][obj.NetID]
+			if uuid then
+				local data = {}
+				local existing = Ext.LoadFile("LeaderLib_UUIDHelper.json")
+				if existing then
+					data = Common.JsonParse(existing)
+					Ext.Print("Updated Osiris Data/LeaderLib_UUIDHelper.json")
+				else
+					Ext.Print("Created Osiris Data/LeaderLib_UUIDHelper.json")
+				end
+				--local name_uuid = string.format("%s_%s", obj.RootTemplate.Name, uuid)
+				local existingEntry = nil
+				for i,v in pairs(data) do
+					if v.UUID == uuid then
+						existingEntry = v
+						break
+					end
+				end
+				if not existingEntry then
+					existingEntry = {}
+					data[#data+1] = existingEntry
+				end
+				existingEntry.UUID = uuid
+				existingEntry.DisplayName = obj.DisplayName
+				existingEntry.RootTemplate = obj.RootTemplate.Id
+				existingEntry.StatsId = GameHelpers.Ext.ObjectIsItem(obj) and obj.StatsId or obj.Stats.Name
+				existingEntry.Tags = StringHelpers.Join(";", obj:GetTags())
+				if GameHelpers.Ext.ObjectIsItem(obj) then
+					existingEntry.WorldPos = obj.WorldPos
+					existingEntry.Rotation = UUID_TO_ROTATION[uuid]
+				end
+				Ext.SaveFile("LeaderLib_UUIDHelper.json", Common.JsonStringify(data))
+			end
+		end
+	end
+
+	local function TryProcessHoverObject(id, handle, hoverType, contextMenu, displayName)
+		if handle then
+			local target = nil
+			if GameHelpers.Ext.ObjectIsCharacter(handle) or GameHelpers.Ext.ObjectIsItem(handle) then
+				target = handle
+			else
+				if hoverType then
+					if hoverType == "Character" then
+						target = GameHelpers.GetCharacter(handle)
+					elseif hoverType == "Item" then
+						target = GameHelpers.GetItem(handle)
+					end
+				else
+					target = GameHelpers.TryGetObject(handle)
+				end
+			end
+			if target then
+				if not hoverType then
+					if GameHelpers.Ext.ObjectIsCharacter(target) then
+						hoverType = "Character"
+					else
+						hoverType = "Item"
+					end
+				end
+				if StringHelpers.IsNullOrEmpty(target.MyGuid) then
+					Ext.PostMessageToServer("LeaderLib_ContextMenu_RequestUUID", Common.JsonStringify({NetID=target.NetID, Type=hoverType}))
+				else
+					NETID_TO_UUID[hoverType][target.NetID] = target.MyGuid
+				end
+				contextMenu:AddBuiltinEntry(id, function(contextMenu, ui, id, actionID, netid)
+					SaveInfoToFile(netid, hoverType)
+				end, displayName, true, true, false, true, target.NetID)
+				return target.NetID
+			end
+		end
+	end
+
+	Ext.RegisterUITypeCall(Data.UIType.characterSheet, "openContextMenu", function (ui, call, itemDouble)
+		sheetItemContextMenuDouble = itemDouble
+	end, "Before")
+	for _,v in pairs(Data.UIType.contextMenu) do
+		Ext.RegisterUITypeCall(v, "menuClosed", function (ui, call)
+			sheetItemContextMenuDouble = nil
+		end)
+		Ext.RegisterUITypeInvokeListener(v, "close", function (ui, call)
+			sheetItemContextMenuDouble = nil
+		end)
+	end
 
 	Ext.RegisterListener("SessionLoaded", function()
 		if not registeredListeners then
@@ -46,53 +160,39 @@ if isClient then
 			UI.ContextMenu.Register.BuiltinOpeningListener(function(contextMenu, ui, this, buttonArr, buttons)
 				if Vars.DebugMode then
 					local cursor = Ext.GetPickingState()
+					local entries = {}
 					if cursor then
-						local target = GameHelpers.TryGetObject(cursor.HoverCharacter or cursor.HoverItem)
-						if target then
-							if StringHelpers.IsNullOrEmpty(target.MyGuid) or GameHelpers.Item.IsObject(target) then
-								Ext.PostMessageToServer("LeaderLib_ContextMenu_RequestUUID", target.NetID)
-							else
-								NETID_TO_UUID[target.NetID] = target.MyGuid
+						entries = {
+							TryProcessHoverObject("LLCM_CopyInfo1", cursor.HoverItem, "Item", contextMenu, "Save Cursor Item to File"),
+							TryProcessHoverObject("LLCM_CopyInfo2", cursor.HoverCharacter, "Character", contextMenu, "Save Cursor Character to File"),
+							TryProcessHoverObject("LLCM_CopyInfo3", cursor.HoverCharacter2, "Character", contextMenu, "Save Cursor Character (2) to File"),
+						}
+						for i=1,#entries-1 do
+							if entries[i] == nil then
+								table.remove(entries, i)
 							end
-							contextMenu:AddBuiltinEntry("LLCM_CopyUUID", function(contextMenu, ui, id, actionID, handle)
-								local uuid = NETID_TO_UUID[handle]
-								if uuid then
-									local obj = GameHelpers.TryGetObject(handle)
-									if obj then
-										local data = {}
-										local existing = Ext.LoadFile("LeaderLib_UUIDHelper.json")
-										if existing then
-											data = Common.JsonParse(existing)
-											Ext.Print("Updated Osiris Data/LeaderLib_UUIDHelper.json")
-										else
-											Ext.Print("Created Osiris Data/LeaderLib_UUIDHelper.json")
-										end
-										--local name_uuid = string.format("%s_%s", obj.RootTemplate.Name, uuid)
-										local existingEntry = nil
-										for i,v in pairs(data) do
-											if v.UUID == uuid then
-												existingEntry = v
-												break
-											end
-										end
-										if not existingEntry then
-											existingEntry = {}
-											data[#data+1] = existingEntry
-										end
-										existingEntry.UUID = uuid
-										existingEntry.DisplayName = obj.DisplayName
-										existingEntry.RootTemplate = obj.RootTemplate.Id
-										existingEntry.StatsId = GameHelpers.Ext.ObjectIsItem(obj) and obj.StatsId or obj.Stats.Name
-										existingEntry.Tags = StringHelpers.Join(";", obj:GetTags())
-										if GameHelpers.Ext.ObjectIsItem(obj) then
-											existingEntry.WorldPos = obj.WorldPos
-											existingEntry.Rotation = NETID_TO_ROTATION[handle]
-										end
-										Ext.SaveFile("LeaderLib_UUIDHelper.json", Common.JsonStringify(data))
-									end
-								end
-							end, "Save Info to File", true, true, false, true, target.NetID)
 						end
+						if cursor.HoverEntity and (cursor.HoverItem ~= cursor.HoverEntity and cursor.HoverCharacter ~= cursor.HoverEntity) then
+							local result = TryProcessHoverObject("LLCM_CopyInfo4", cursor.HoverEntity, nil, contextMenu, "Save Cursor Entity to File")
+							if result then
+								table.insert(entries, result)
+							end
+						end
+					end
+					if sheetItemContextMenuDouble then
+						local item = Ext.GetItem(Ext.DoubleToHandle(sheetItemContextMenuDouble))
+						if item then
+							TryProcessHoverObject("LLCM_CopyEquipmentInfo", item, "Item", contextMenu, "Save Cursor EQ to File")
+						end
+					end
+					if #entries > 1 then
+						contextMenu:AddBuiltinEntry("LLCM_CopyInfoAll", function(contextMenu, ui, id, actionID, none)
+							for i,v in pairs(entries) do
+								if v ~= nil then
+									SaveInfoToFile(v)
+								end
+							end
+						end, "Save All Cursor Info to File", true, true, false, true)
 					end
 				end
 			end)
@@ -102,18 +202,27 @@ if isClient then
 	end)
 
 	Ext.RegisterNetListener("LeaderLib_ContextMenu_SetUUID", function(cmd, payload)
+		print(cmd,payload)
 		local data = Common.JsonParse(payload)
 		if data then
-			NETID_TO_UUID[data.NetID] = data.UUID
-			NETID_TO_ROTATION[data.NetID] = data.Rotation
+			NETID_TO_UUID[data.Type][data.NetID] = data.UUID
+			UUID_TO_ROTATION[data.UUID] = data.Rotation
 		end
 	end)
 else
 	Ext.RegisterNetListener("LeaderLib_ContextMenu_RequestUUID", function(cmd, payload, userid)
-		local netid = tonumber(payload)
-		local object = GameHelpers.TryGetObject(netid)
-		if object then
-			GameHelpers.Net.PostToUser(userid, "LeaderLib_ContextMenu_SetUUID", Common.JsonStringify({NetID = netid, UUID = object.MyGuid, Rotation={GetRotation(object.MyGuid)}}))
+		print(cmd,payload)
+		local data = Common.JsonParse(payload)
+		if data then
+			local object = GameHelpers.TryGetObject(data.NetID)
+			if object then
+				GameHelpers.Net.PostToUser(userid, "LeaderLib_ContextMenu_SetUUID", {
+					NetID = object.NetID,
+					UUID = object.MyGuid,
+					Type = data.Type,
+					Rotation={GetRotation(object.MyGuid)}
+				})
+			end
 		end
 	end)
 end

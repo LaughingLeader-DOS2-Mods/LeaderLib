ca1462955cb7aa086c6f4bddc835cb2ae2abce33
LaughingLeader <laughingleader@gmail.com>
2022-02-03 17:15:57 -0600

    Add a dump command for character.Visual

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Debug/SharedCommands.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Debug/SharedCommands.lua
@@ -20,6 +20,7 @@ if _EXTVERSION >= 56 then
 			if not isClient then
 				if Ext.Effect then
 					Ext.IO.SaveFile("Dumps/Effects.json", Ext.DumpExport(Ext.Effect.GetAllEffectHandles()))
+					Ext.Print("[dump:effect] Saved effects to Dumps/Effects.json")
 				else
 					Ext.PrintWarning("[dump:effect] Ext.Effect is nil!")
 				end
@@ -32,9 +33,18 @@ if _EXTVERSION >= 56 then
 				local wiz = Ext.UI.GetCharacterCreationWizard()
 				if wiz then
 					Ext.IO.SaveFile("Dumps/CC_Wizard.json", Ext.DumpExport(wiz))
+					Ext.Print("[dump:cc] Saved data to Dumps/CC_Wizard.json")
 				else
 					Ext.PrintWarning("[dump:cc] No character creation wizard is available.")
 				end
+			else
+				SendDumpCommand(...)
+			end
+		end,
+		visual = function (...)
+			if isClient then
+				Ext.IO.SaveFile("Dumps/Visual.json", Ext.DumpExport(Client:GetCharacter().Visual))
+				Ext.Print("[dump:cc] Saved visual data to Dumps/Visual.json")
 			else
 				SendDumpCommand(...)
 			end
@@ -47,7 +57,7 @@ if _EXTVERSION >= 56 then
 			return
 		end
 		for k,func in pairs(_DUMP) do
-			if string.find(k, dumpType) then
+			if string.find(dumpType, k) then
 				func(dumpType, ...)
 			end
 		end
@@ -55,7 +65,7 @@ if _EXTVERSION >= 56 then
 
 	Ext.RegisterConsoleCommand("dump", OnDumpCommand)
 
-	Ext.RegisterNetListener("LeaderLib_Debug_RunDumpCommand", function (net, payload)
+	Ext.RegisterNetListener("LeaderLib_Debug_RunDumpCommand", function (netmsg, payload)
 		local cmdData = Common.JsonParse(payload)
 		if cmdData then
 			OnDumpCommand(cmdData.Command, table.unpack(cmdData.Args))

b38d0447396b65c085c29309f57a172f6c818ad5
LaughingLeader <laughingleader@gmail.com>
2022-02-23 20:02:31 -0600

    Fix WeaponEx patch on the server side, move check into a table format

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Patches/ModPatching.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Patches/ModPatching.lua
@@ -1,74 +1,77 @@
 local isClient = Ext.IsClient()
-local function PatchMods()
-	if isClient then
-				--Weapon Expansion
-		if Ext.IsModLoaded("c60718c3-ba22-4702-9c5d-5ad92b41ba5f")
-		and Ext.GetModInfo("c60718c3-ba22-4702-9c5d-5ad92b41ba5f").Version <= 153288705
-		then
-			if Ext.Version() < 56 then
-				Ext._NetListeners["LLWEAPONEX_SetWorldTooltipText"] = nil
+
+local Patches = {
+	--Weapon Expansion
+	["c60718c3-ba22-4702-9c5d-5ad92b41ba5f"] = {
+		Version = 153288705,
+		Patch = function ()
+			if isClient then
+				if Ext.Version() < 56 then
+					Ext._NetListeners["LLWEAPONEX_SetWorldTooltipText"] = nil
+				else
+					Ext._Internal._NetListeners["LLWEAPONEX_SetWorldTooltipText"] = nil
+				end
+	
+				Ext.RegisterNetListener("LLWEAPONEX_SetWorldTooltipText", function (cmd, payload)
+					local ui = Ext.GetUIByType(Data.UIType.tooltip)
+					if ui then
+						local main = ui:GetRoot()
+						if main ~= nil then
+							local text = payload or ""
+							if main.tf ~= nil then
+								main.tf.shortDesc = text
+								if main.tf.setText ~= nil then
+									main.tf.setText(text,0)
+								end
+							elseif main.defaultTooltip ~= nil then
+								main.defaultTooltip.shortDesc = text
+								if main.defaultTooltip.setText ~= nil then
+									main.defaultTooltip.setText(text,0)
+								end
+							end
+						end
+					end
+				end)
 			else
-				Ext._Internal._NetListeners["LLWEAPONEX_SetWorldTooltipText"] = nil	
-			end
-
-			Ext.RegisterNetListener("LLWEAPONEX_SetWorldTooltipText", function (cmd, payload)
-				local ui = Ext.GetUIByType(Data.UIType.tooltip)
-				if ui then
-					local main = ui:GetRoot()
-					if main ~= nil then
-						local text = payload or ""
-						if main.tf ~= nil then
-							main.tf.shortDesc = text
-							if main.tf.setText ~= nil then
-								main.tf.setText(text,0)
+				--Harken = "e446752a-13cc-4a88-a32e-5df244c90d8b",
+				--Korvash = "3f20ae14-5339-4913-98f1-24476861ebd6",
+				local uuid = "e446752a-13cc-4a88-a32e-5df244c90d8b"
+				if ObjectExists(uuid) == 1 then
+					local faction = GetFaction(uuid)
+					if StringHelpers.IsNullOrWhitespace(faction) then
+						if GameHelpers.Character.IsPlayer(uuid) then
+							SetFaction(uuid, "Hero LLWEAPONEX_Harken")
+							if StringHelpers.IsNullOrWhitespace(GetFaction(uuid)) then
+								SetFaction(uuid, "Hero Henchman Fighter")
 							end
-						elseif main.defaultTooltip ~= nil then
-							main.defaultTooltip.shortDesc = text
-							if main.defaultTooltip.setText ~= nil then
-								main.defaultTooltip.setText(text,0)
+						else
+							SetFaction(uuid, "Good NPC")
+						end
+					end
+				end
+				uuid = "3f20ae14-5339-4913-98f1-24476861ebd6"
+				if ObjectExists(uuid) == 1 then
+					local faction = GetFaction(uuid)
+					if StringHelpers.IsNullOrWhitespace(faction) then
+						if GameHelpers.Character.IsPlayer(uuid) then
+							SetFaction(uuid, "Hero LLWEAPONEX_Korvash")
+							if StringHelpers.IsNullOrWhitespace(GetFaction(uuid)) then
+								SetFaction(uuid, "Hero Henchman Inquisitor")
 							end
+						else
+							SetFaction(uuid, "Good NPC")
 						end
-					end
-				end
-			end)
-
-			--Harken = "e446752a-13cc-4a88-a32e-5df244c90d8b",
-			--Korvash = "3f20ae14-5339-4913-98f1-24476861ebd6",
-			local uuid = "e446752a-13cc-4a88-a32e-5df244c90d8b"
-			if ObjectExists(uuid) == 1 then
-				local faction = GetFaction(uuid)
-				if StringHelpers.IsNullOrWhitespace(faction) then
-					if GameHelpers.Character.IsPlayer(uuid) then
-						SetFaction(uuid, "Hero LLWEAPONEX_Harken")
-						if StringHelpers.IsNullOrWhitespace(GetFaction(uuid)) then
-							SetFaction(uuid, "Hero Henchman Fighter")
-						end
-					else
-						SetFaction(uuid, "Good NPC")
 					end
 				end
 			end
-			uuid = "3f20ae14-5339-4913-98f1-24476861ebd6"
-			if ObjectExists(uuid) == 1 then
-				local faction = GetFaction(uuid)
-				if StringHelpers.IsNullOrWhitespace(faction) then
-					if GameHelpers.Character.IsPlayer(uuid) then
-						SetFaction(uuid, "Hero LLWEAPONEX_Korvash")
-						if StringHelpers.IsNullOrWhitespace(GetFaction(uuid)) then
-							SetFaction(uuid, "Hero Henchman Inquisitor")
-						end
-					else
-						SetFaction(uuid, "Good NPC")
-					end
-				end
-			end
+		end
+	}
+}
 
-			-- Ext.Events.NetMessageReceived:Subscribe(function (e)
-			-- 	if e.Channel == "LLWEAPONEX_SetWorldTooltipText" then
-			-- 		e:StopPropagation()
-					
-			-- 	end
-			-- end)
+local function PatchMods()
+	for uuid,data in pairs(Patches) do
+		if Ext.IsModLoaded(uuid) and Ext.GetModInfo(uuid).Version <= data.Version then
+			data.Patch()
 		end
 	end
 end

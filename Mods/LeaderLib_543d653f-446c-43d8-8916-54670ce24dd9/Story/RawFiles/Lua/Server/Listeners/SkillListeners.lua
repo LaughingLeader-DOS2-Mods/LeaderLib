315baefd20c41de7dc05e4be34b385b69953e9d5
LaughingLeader <laughingleader@gmail.com>
2022-05-07 18:19:47 -0500

    Reorganize skill listeners inside the SkillManager, and support a new API (passing EsvCharacter)

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Listeners/SkillListeners.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/Listeners/SkillListeners.lua
@@ -1,57 +1,3 @@
-if SkillSystem == nil then
-	SkillSystem = {}
-end
-
-local ignoreSkill = {}
-
---- Gets the base skill from a skill.
---- @param skill string The skill entry to check.
---- @return string The base skill, if any, otherwise the skill that was passed in.
-function SkillSystem.GetBaseSkill(skill, match)
-	if skill ~= nil then
-		local checkParent = true
-		if match ~= nil and match ~= "" and not string.find(skill, match) then
-			checkParent = false
-		end
-		if checkParent then
-			local skill = Ext.StatGetAttribute(skill, "Using")
-			if skill ~= nil then
-				return SkillSystem.GetBaseSkill(skill, match)
-			end
-		end
-	end
-	return skill
-end
-
-local function GetListeners(skill)
-	local parsingAllTable = false
-	local listeners = SkillListeners[skill]
-	if listeners == nil then
-		listeners = SkillListeners["All"]
-		parsingAllTable = true
-	end
-	if listeners then
-		local i = 0
-		local count = #listeners
-		return function ()
-			i = i + 1
-			if not parsingAllTable and i == count+1 then
-				if SkillListeners["All"] ~= nil then
-					listeners = SkillListeners["All"]
-					i = 1
-					count = #listeners
-					parsingAllTable = true
-				end
-			end
-
-			if i <= count then
-				return listeners[i]
-			end
-		end
-	end
-	return function() end
-end
-
 ---A temporary table used to store data for a skill, including targets / skill information.
 ---@type table<string,SkillEventData>
 local skillEventDataTable = {}
@@ -77,23 +23,6 @@ local function GetCharacterSkillData(skill, uuid, createIfMissing, skillType, sk
 	end
 	PersistentVars.SkillData[uuid] = data:Serialize()
 	return data
-end
-
-function SkillSystem.LoadSaveData()
-	if PersistentVars.SkillData then
-		for uuid,tbl in pairs(PersistentVars.SkillData) do
-			if ObjectExists(uuid) == 1 and not StringHelpers.IsNullOrWhitespace(tbl.Skill) and NRD_StatExists(tbl.Skill) then
-				local data = Classes.SkillEventData:Create(uuid, "", "", "")
-				data:LoadFromSave(tbl)
-				if skillEventDataTable[data.Skill] == nil then
-					skillEventDataTable[data.Skill] = {}
-				end
-				skillEventDataTable[data.Skill][uuid] = data
-			else
-				PersistentVars.SkillData[uuid] = nil
-			end
-		end
-	end
 end
 
 local function RemoveCharacterSkillData(uuid, skill)
@@ -145,12 +74,12 @@ function OnSkillPreparing(char, skillprototype)
 	-- end
 	local last = PersistentVars.IsPreparingSkill[char]
 	if last and last ~= skill then
-		SkillSystem.OnSkillPreparingCancel(char, "", last, true)
+		SkillManager.OnSkillPreparingCancel(char, "", last, true)
 	end
 
 	if not last or last ~= skill then
 		local skillData = Ext.GetStat(skill)
-		for callback in GetListeners(skill) do
+		for callback in SkillManager.GetListeners(skill) do
 			--PrintDebug("[LeaderLib_SkillListeners.lua:OnSkillPreparing] char(",char,") skillprototype(",skillprototype,") skill(",skill,")")
 			local status,err = xpcall(callback, debug.traceback, skill, char, SKILL_STATE.PREPARE, skillData, "StatEntrySkillData")
 			if not status then
@@ -163,62 +92,6 @@ function OnSkillPreparing(char, skillprototype)
 	RemoveCharacterSkillData(char)
 	PersistentVars.IsPreparingSkill[char] = skill
 end
-
-function SkillSystem.OnSkillPreparingCancel(char, skillprototype, skill, skipRemoval)
-	skill = skill or GetSkillEntryName(skillprototype)
-	local skillData = Ext.GetStat(skill)
-	for callback in GetListeners(skill) do
-		--PrintDebug("[LeaderLib_SkillListeners.lua:OnSkillPreparing] char(",char,") skillprototype(",skillprototype,") skill(",skill,")")
-		local status,err = xpcall(callback, debug.traceback, skill, char, SKILL_STATE.CANCEL, skillData, "StatEntrySkillData")
-		if not status then
-			Ext.PrintError("[LeaderLib_SkillListeners] Error invoking function:\n", err)
-		end
-	end
-
-	if skipRemoval ~= true then
-		RemoveCharacterSkillData(char)
-	end
-end
-
-function SkillSystem.CheckPreparingState(uuid)
-	local last = PersistentVars.IsPreparingSkill[uuid]
-	if last then
-		local action = NRD_CharacterGetCurrentAction(uuid) or ""
-		local skill = GetSkillEntryName(NRD_ActionStateGetString(uuid, "SkillId") or "")
-		if StringHelpers.IsNullOrEmpty(skill) or (action ~= "PrepareSkill" and action ~= "UseSkill") or skill ~= last then
-			SkillSystem.OnSkillPreparingCancel(uuid, "", last)
-		end
-	end
-end
-
---When the ActionCancel button is pressed.
-Ext.RegisterNetListener("LeaderLib_Input_OnActionCancel", function(cmd, payload)
-	if not StringHelpers.IsNullOrEmpty(payload) then
-		local netid = tonumber(payload)
-		local character = Ext.GetCharacter(netid)
-		if character then
-			local action = NRD_CharacterGetCurrentAction(character.MyGuid) or ""
-			if action == "PrepareSkill" then
-				local skillPrototype = NRD_ActionStateGetString(character.MyGuid, "SkillId")
-				if not StringHelpers.IsNullOrEmpty(skillPrototype) then
-					SkillSystem.OnSkillPreparingCancel(character.MyGuid, skillPrototype)
-				end
-			end
-		end
-	end
-end)
-
---When the active skill on hotBar or bottomBar_c is cleared
-Ext.RegisterNetListener("LeaderLib_OnActiveSkillCleared", function(cmd, id)
-	if not StringHelpers.IsNullOrWhitespace(id) then
-		local character = Ext.GetCharacter(tonumber(id))
-		if character then
-			Timer.StartOneshot("Timers_LeaderLib_OnActiveSkillCleared", 50, function()
-				SkillSystem.CheckPreparingState(character.MyGuid)
-			end)
-		end
-	end
-end)
 
 -- Fires when CharacterUsedSkill fires. This happens after all the target events.
 function OnSkillUsed(char, skill, skillType, skillAbility)
@@ -238,10 +111,11 @@ function OnSkillUsed(char, skill, skillType, skillAbility)
 	if data then
 		--Quake doesn't fire any target events, but works like a shout
 		if skillType == "quake" then
-			data:AddTargetPosition(GetPosition(char))
+			local x,y,z = GetPosition(char)
+			data:AddTargetPosition(x,y,z)
 		end
 		local status,err = nil,nil
-		for callback in GetListeners(skill) do
+		for callback in SkillManager.GetListeners(skill) do
 			if Vars.DebugMode then
 				--PrintDebug("[LeaderLib_SkillListeners.lua:OnSkillUsed] char(",char,") skill(",skill,") data(",data:ToString(),")")
 				--PrintDebug("params(",Common.JsonStringify({...}),")")
@@ -260,7 +134,7 @@ function OnSkillCast(char, skill, skilLType, skillAbility)
 	---@type SkillEventData
 	local data = GetCharacterSkillData(skill, uuid, true, skilLType, skillAbility, Vars.DebugMode, "OnSkillCast")
 	if data ~= nil then
-		for callback in GetListeners(skill) do
+		for callback in SkillManager.GetListeners(skill) do
 			local b,err = xpcall(callback, debug.traceback, skill, uuid, SKILL_STATE.CAST, data, data.Type)
 			if not b then
 				Ext.PrintError("[LeaderLib:SkillListeners:OnSkillCast] Error invoking function:\n", err)
@@ -292,7 +166,7 @@ end
 --- @param data HitData|ProjectileHitData
 function OnSkillHit(skillId, target, source, damage, hit, context, hitStatus, data)
 	if not IgnoreHitTarget(target.MyGuid) then
-		for callback in GetListeners(skillId) do
+		for callback in SkillManager.GetListeners(skillId) do
 			local b,err = xpcall(callback, debug.traceback, skillId, source.MyGuid, SKILL_STATE.HIT, data, data.Type)
 			if not b then
 				Ext.PrintError("[LeaderLib_SkillListeners] Error invoking function:\n", err)
@@ -314,7 +188,7 @@ RegisterProtectedExtenderListener("ProjectileHit", function (projectile, hitObje
 			local target = hitObject ~= nil and hitObject.MyGuid or ""
 			---@type ProjectileHitData
 			local data = Classes.ProjectileHitData:Create(target, uuid, projectile, position, skill)
-			for callback in GetListeners(skill) do
+			for callback in SkillManager.GetListeners(skill) do
 				local b,err = xpcall(callback, debug.traceback, skill, uuid, SKILL_STATE.PROJECTILEHIT, data, data.Type)
 				if not b then
 					Ext.PrintError("[LeaderLib:SkillListeners:ProjectileHit] Error invoking function:\n", err)
@@ -331,7 +205,7 @@ RegisterProtectedExtenderListener("BeforeShootProjectile", function (request)
 	if not StringHelpers.IsNullOrEmpty(skill) and request.Source then
 		local object = Ext.GetGameObject(request.Source)
 		if object then
-			for callback in GetListeners(skill) do
+			for callback in SkillManager.GetListeners(skill) do
 				local b,err = xpcall(callback, debug.traceback, skill, object.MyGuid, SKILL_STATE.BEFORESHOOT, request, "EsvShootProjectileRequest")
 				if not b then
 					Ext.PrintError("[LeaderLib:SkillListeners:BeforeShootProjectile] Error invoking function:\n", err)
@@ -348,7 +222,7 @@ RegisterProtectedExtenderListener("ShootProjectile", function (projectile)
 	if not StringHelpers.IsNullOrEmpty(skill) and projectile.CasterHandle then
 		local object = Ext.GetGameObject(projectile.CasterHandle)
 		if object then
-			for callback in GetListeners(skill) do
+			for callback in SkillManager.GetListeners(skill) do
 				local b,err = xpcall(callback, debug.traceback, skill, object.MyGuid, SKILL_STATE.SHOOTPROJECTILE, projectile, "EsvProjectile")
 				if not b then
 					Ext.PrintError("[LeaderLib:SkillListeners:BeforeShootProjectile] Error invoking function:\n", err)
@@ -362,7 +236,7 @@ end)
 RegisterProtectedOsirisListener("SkillAdded", Data.OsirisEvents.SkillAdded, "after", function(uuid, skill, learned)
 	uuid = StringHelpers.GetUUID(uuid)
 	learned = learned == 1 and true or false
-	for callback in GetListeners(skill) do
+	for callback in SkillManager.GetListeners(skill) do
 		local b,err = xpcall(callback, debug.traceback, skill, uuid, SKILL_STATE.LEARNED, learned, "boolean")
 		if not b then
 			Ext.PrintError("[LeaderLib:SkillListeners:SkillAdded] Error invoking function:\n", err)
@@ -380,7 +254,7 @@ RegisterProtectedOsirisListener("SkillActivated", Data.OsirisEvents.SkillActivat
 			learned = skillInfo.IsLearned or skillInfo.ZeroMemory
 		end
 	end
-	for callback in GetListeners(skill) do
+	for callback in SkillManager.SkillManager.GetListeners(skill) do
 		local b,err = xpcall(callback, debug.traceback, skill, uuid, SKILL_STATE.MEMORIZED, learned, "boolean")
 		if not b then
 			Ext.PrintError("[LeaderLib:SkillListeners:SkillActivated] Error invoking function:\n", err)
@@ -401,7 +275,7 @@ RegisterProtectedOsirisListener("SkillDeactivated", Data.OsirisEvents.SkillDeact
 			learned = skillInfo.IsLearned or skillInfo.ZeroMemory
 		end
 	end
-	for callback in GetListeners(skill) do
+	for callback in SkillManager.GetListeners(skill) do
 		local b,err = xpcall(callback, debug.traceback, skill, uuid, SKILL_STATE.UNMEMORIZED, learned, "boolean")
 		if not b then
 			Ext.PrintError("[LeaderLib:SkillListeners:SkillDeactivated] Error invoking function:\n", err)
---
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/System/SkillManager.lua
@@ -0,0 +1,304 @@
+if SkillManager == nil then
+	SkillManager = {}
+end
+
+---A temporary table used to store data for a skill, including targets / skill information.
+---@type table<string,SkillEventData>
+local skillEventDataTable = {}
+
+---@private
+SkillManager.SkillEventDataTable = skillEventDataTable
+
+---@private
+function SkillManager.RemoveCharacterSkillData(uuid, skill)
+	if skill ~= nil then
+		local skillDataHolder = skillEventDataTable[skill]
+		if skillDataHolder ~= nil then
+			skillDataHolder[uuid] = nil
+		end
+	else
+		-- Remove everything for this character
+		for skill,data in pairs(skillEventDataTable) do
+			if data[uuid] ~= nil then
+				data[uuid] = nil
+			end
+		end
+	end
+	PersistentVars.IsPreparingSkill[uuid] = nil
+	PersistentVars.SkillData[uuid] = nil
+end
+
+---@private
+---@param skill string
+---@return fun():LeaderLibSkillListenerCallback
+function SkillManager.GetListeners(skill)
+	local parsingAllTable = false
+	local listeners = SkillListeners[skill]
+	if listeners == nil then
+		listeners = SkillListeners["All"]
+		parsingAllTable = true
+	end
+	if listeners then
+		local i = 0
+		local count = #listeners
+		return function ()
+			i = i + 1
+			if not parsingAllTable and i == count+1 then
+				if SkillListeners["All"] ~= nil then
+					listeners = SkillListeners["All"]
+					i = 1
+					count = #listeners
+					parsingAllTable = true
+				end
+			end
+
+			if i <= count then
+				return listeners[i]
+			end
+		end
+	end
+	return function() end
+end
+
+--- Gets the base skill from a skill.
+--- @param skill string The skill entry to check.
+--- @return string The base skill, if any, otherwise the skill that was passed in.
+function SkillManager.GetBaseSkill(skill, match)
+	if skill ~= nil then
+		local checkParent = true
+		if match ~= nil and match ~= "" and not string.find(skill, match) then
+			checkParent = false
+		end
+		if checkParent then
+			local skill = Ext.StatGetAttribute(skill, "Using")
+			if skill ~= nil then
+				return SkillManager.GetBaseSkill(skill, match)
+			end
+		end
+	end
+	return skill
+end
+
+function SkillManager.LoadSaveData()
+	if PersistentVars.SkillData then
+		for uuid,tbl in pairs(PersistentVars.SkillData) do
+			if ObjectExists(uuid) == 1 and not StringHelpers.IsNullOrWhitespace(tbl.Skill) and NRD_StatExists(tbl.Skill) then
+				local data = Classes.SkillEventData:Create(uuid, "", "", "")
+				data:LoadFromSave(tbl)
+				if skillEventDataTable[data.Skill] == nil then
+					skillEventDataTable[data.Skill] = {}
+				end
+				skillEventDataTable[data.Skill][uuid] = data
+			else
+				PersistentVars.SkillData[uuid] = nil
+			end
+		end
+	end
+end
+
+function SkillManager.OnSkillPreparingCancel(char, skillprototype, skill, skipRemoval)
+	skill = skill or StringHelpers.GetSkillEntryName(skillprototype)
+	local skillData = Ext.GetStat(skill)
+	for callback in SkillManager.GetListeners(skill) do
+		--PrintDebug("[LeaderLib_SkillListeners.lua:OnSkillPreparing] char(",char,") skillprototype(",skillprototype,") skill(",skill,")")
+		local status,err = xpcall(callback, debug.traceback, skill, char, SKILL_STATE.CANCEL, skillData, "StatEntrySkillData")
+		if not status then
+			Ext.PrintError("[LeaderLib_SkillListeners] Error invoking function:\n", err)
+		end
+	end
+
+	if skipRemoval ~= true then
+		SkillManager.RemoveCharacterSkillData(char)
+	end
+end
+
+function SkillManager.CheckPreparingState(uuid)
+	local last = PersistentVars.IsPreparingSkill[uuid]
+	if last then
+		local action = NRD_CharacterGetCurrentAction(uuid) or ""
+		local skill = StringHelpers.GetSkillEntryName(NRD_ActionStateGetString(uuid, "SkillId") or "")
+		if StringHelpers.IsNullOrEmpty(skill) or (action ~= "PrepareSkill" and action ~= "UseSkill") or skill ~= last then
+			SkillManager.OnSkillPreparingCancel(uuid, "", last)
+		end
+	end
+end
+
+--When the ActionCancel button is pressed.
+Ext.RegisterNetListener("LeaderLib_Input_OnActionCancel", function(cmd, payload)
+	if not StringHelpers.IsNullOrEmpty(payload) then
+		local netid = tonumber(payload)
+		local character = Ext.GetCharacter(netid)
+		if character then
+			local action = NRD_CharacterGetCurrentAction(character.MyGuid) or ""
+			if action == "PrepareSkill" then
+				local skillPrototype = NRD_ActionStateGetString(character.MyGuid, "SkillId")
+				if not StringHelpers.IsNullOrEmpty(skillPrototype) then
+					SkillManager.OnSkillPreparingCancel(character.MyGuid, skillPrototype)
+				end
+			end
+		end
+	end
+end)
+
+--When the active skill on hotBar or bottomBar_c is cleared
+Ext.RegisterNetListener("LeaderLib_OnActiveSkillCleared", function(cmd, id)
+	if not StringHelpers.IsNullOrWhitespace(id) then
+		local character = Ext.GetCharacter(tonumber(id))
+		if character then
+			Timer.StartOneshot("Timers_LeaderLib_OnActiveSkillCleared", 50, function()
+				SkillManager.CheckPreparingState(character.MyGuid)
+			end)
+		end
+	end
+end)
+
+SkillManager.Register = {}
+
+local _ListenersToRegister = {}
+local _SessionInitialized = false
+setmetatable(_ListenersToRegister, {__mode =" kv"})
+
+local function SortListenersByPriority(a,b)
+	return a.Priority < b.Priority
+end
+
+Ext.RegisterListener("SessionLoaded", function ()
+	local count = #_ListenersToRegister
+	if count > 0 then
+		table.sort(_ListenersToRegister, SortListenersByPriority)
+		for i=1,count do
+			local entry = _ListenersToRegister[i]
+			if entry.Callback ~= nil then
+				RegisterSkillListener(entry.Skill, entry.Callback)
+			end
+		end
+		_ListenersToRegister = {}
+	end
+	_SessionInitialized = true
+end)
+
+---@alias SkillManagerAllStateCallback fun(skill:string, caster:EsvCharacter, state:SKILL_STATE, data:SkillEventData|HitData|ProjectileHitData|StatEntrySkillData|EsvProjectile|boolean, dataType:LeaderLibSkillListenerDataType)
+---@alias SkillManagerSkillEventCallback fun(skill:string, caster:EsvCharacter, state:SKILL_STATE, data:SkillEventData, dataType:LeaderLibSkillListenerDataType)
+---@alias SkillManagerHitCallback fun(skill:string, caster:EsvCharacter, state:SKILL_STATE, data:HitData, dataType:LeaderLibSkillListenerDataType)
+---@alias SkillManagerProjectileHitCallback fun(skill:string, caster:EsvCharacter, state:SKILL_STATE, data:ProjectileHitData, dataType:LeaderLibSkillListenerDataType)
+---@alias SkillManagerProjectileShootCallback fun(skill:string, caster:EsvCharacter, state:SKILL_STATE, data:EsvProjectile, dataType:LeaderLibSkillListenerDataType)
+
+---@param state SKILL_STATE
+---@param matchState SKILL_STATE|SKILL_STATE[]
+---@param matchStateType string
+---@return boolean
+local function SkillStateMatches(state, matchState, matchStateType)
+	if not matchState then
+		return true
+	end
+	if matchStateType == "table" then
+		return TableHelpers.HasValue(matchState, state)
+	elseif matchStateType == "string" then
+		return matchState == state
+	end
+	return true
+end
+
+---Registers a function to call when a specific skill or array of skills has a skill event. This function fires for all skill events unless otherwise specified.
+---@param skill string|string[]
+---@param callback SkillManagerAllStateCallback
+---@param onlySkillState SKILL_STATE|SKILL_STATE[]|nil If set, the callback will only fire for specified skill states.
+---@param priority integer|nil Optional listener priority
+---@param once boolean|nil If true, the listener will fire once, and then get removed. Use with onlySkillState to ensure it only fires for the specific state.
+function SkillManager.Register.All(skill, callback, onlySkillState, priority, once)
+	---@type LeaderLibSkillListenerCallback
+	local callbackWrapper = nil
+	if once then
+		if not onlySkillState then
+			callbackWrapper = function(id, uuid, state, data, dataType)
+				local caster = Ext.GetGameObject(uuid)
+				local b,err = xpcall(callback, debug.traceback, id, caster, state, data, dataType)
+				if not b then
+					Ext.PrintError(err)
+				end
+				RemoveSkillListener(skill, callbackWrapper)
+			end
+		else
+			local matchStateType = type(onlySkillState)
+			callbackWrapper = function(id, uuid, state, data, dataType)
+				if not SkillStateMatches(state, onlySkillState, matchStateType) then
+					return
+				end
+				local caster = Ext.GetGameObject(uuid)
+				local b,err = xpcall(callback, debug.traceback, id, caster, state, data, dataType)
+				if not b then
+					Ext.PrintError(err)
+				end
+				RemoveSkillListener(skill, callbackWrapper)
+			end
+		end
+	else
+		if not onlySkillState then
+			callbackWrapper = function(id, uuid, state, data, dataType)
+				local caster = Ext.GetGameObject(uuid)
+				local b,err = xpcall(callback, debug.traceback, id, caster, state, data, dataType)
+				if not b then
+					Ext.PrintError(err)
+				end
+			end
+		else
+			local matchStateType = type(onlySkillState)
+			callbackWrapper = function(id, uuid, state, data, dataType)
+				if not SkillStateMatches(state, onlySkillState, matchStateType) then
+					return
+				end
+				local caster = Ext.GetGameObject(uuid)
+				local b,err = xpcall(callback, debug.traceback, id, caster, state, data, dataType)
+				if not b then
+					Ext.PrintError(err)
+				end
+			end
+		end
+	end
+	if not _SessionInitialized then
+		table.insert(_ListenersToRegister, {
+			Callback = callbackWrapper,
+			Skill = skill,
+			Priority = priority or 100
+		})
+	else
+		--TODO Store priorities somewhere for use later here? Support registering skill listeners after the fact.
+		RegisterSkillListener(skill, callbackWrapper)
+	end
+end
+
+---Registers a function to call when a specific skill or array of skills has a SKILL_STATE.HIT event.
+---@param skill string|string[]
+---@param callback SkillManagerHitCallback
+---@param priority integer|nil Optional listener priority
+---@param once boolean|nil If true, the listener will fire once, and then get removed. Use with onlySkillState to ensure it only fires for the specific state.
+function SkillManager.Register.Hit(skill, callback, priority, once)
+	SkillManager.Register.All(skill, callback, SKILL_STATE.HIT, priority, once)
+end
+
+---Registers a function to call when a specific skill or array of skills has a SKILL_STATE.PROJECTILEHIT event.
+---@param skill string|string[]
+---@param callback SkillManagerHitCallback
+---@param priority integer|nil Optional listener priority
+---@param once boolean|nil If true, the listener will fire once, and then get removed. Use with onlySkillState to ensure it only fires for the specific state.
+function SkillManager.Register.ProjectileHit(skill, callback, priority, once)
+	SkillManager.Register.All(skill, callback, SKILL_STATE.PROJECTILEHIT, priority, once)
+end
+
+---Registers a function to call when a specific skill or array of skills has a SKILL_STATE.USED event.
+---@param skill string|string[]
+---@param callback SkillManagerSkillEventCallback
+---@param priority integer|nil Optional listener priority
+---@param once boolean|nil If true, the listener will fire once, and then get removed. Use with onlySkillState to ensure it only fires for the specific state.
+function SkillManager.Register.Used(skill, callback, priority, once)
+	SkillManager.Register.All(skill, callback, SKILL_STATE.USED, priority, once)
+end
+
+---Registers a function to call when a specific skill or array of skills has a SKILL_STATE.CAST event.
+---@param skill string|string[]
+---@param callback SkillManagerSkillEventCallback
+---@param priority integer|nil Optional listener priority
+---@param once boolean|nil If true, the listener will fire once, and then get removed. Use with onlySkillState to ensure it only fires for the specific state.
+function SkillManager.Register.Cast(skill, callback, priority, once)
+	SkillManager.Register.All(skill, callback, SKILL_STATE.CAST, priority, once)
+end

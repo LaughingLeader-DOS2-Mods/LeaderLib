c438e64d945caf39922fcc98e062251bab66487c
LaughingLeader <laughingleader@gmail.com>
2022-05-10 03:05:20 -0500

    Continue implementing EffectManager functions for playing/preserving effects

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/System/EffectManager.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Server/System/EffectManager.lua
@@ -4,6 +4,36 @@ if EffectManager == nil then
 	EffectManager = {}
 end
 
+local _INTERNAL = {}
+
+EffectManager._Internal = _INTERNAL
+
+---@class EffectManagerEsvEffectParams
+---@field BeamTarget ObjectHandle
+---@field BeamTargetBone string
+---@field BeamTargetPos number[]
+---@field Bone string
+---@field DetachBeam boolean
+---@field Duration number
+---@field EffectName string
+---@field ForgetEffect boolean
+---@field IsDeleted boolean
+---@field IsForgotten boolean
+---@field Loop boolean
+---@field Position number[]
+---@field Rotation number[]
+---@field Scale number
+---@field Target ObjectHandle
+
+local ObjectHandleEffectParams = {
+	BeamTarget = true,
+	Target = true
+}
+
+---@class EffectManagerEsvEffect:EffectManagerEsvEffectParams
+---@field NetID NETID
+---@field Delete fun(self:EffectManagerEsvEffect)
+
 ---@alias LeaderLibEffectManagerTarget UUID|NETID|EsvCharacter|EsvItem|number[]
 
 ---@class LeaderLibObjectLoopEffectSaveData
@@ -13,16 +43,35 @@ end
 
 ---@param uuid string
 ---@param effect string
----@param bone string
----@param handle integer
-function EffectManager.SaveObjectEffectData(uuid, effect, bone, handle)
+---@param params EffectManagerEsvEffectParams
+function _INTERNAL.SaveObjectEffectData(uuid, effect, params)
 	if PersistentVars.ObjectLoopEffects[uuid] == nil then
 		PersistentVars.ObjectLoopEffects[uuid] = {}
 	end
+	local savedParams = nil
+	if type(params) == "table" then
+		savedParams = {}
+		local hasParams = false
+		for k,v in pairs(params) do
+			local t = type(v)
+			if t == "boolean" or t == "number" or t == "string" or t == "table" then
+				savedParams[k] = v
+				hasParams = true
+			elseif ObjectHandleEffectParams[k] then
+				local obj = Ext.GetGameObject(v)
+				if obj then
+					savedParams[k] = obj.MyGuid
+					hasParams = true
+				end
+			end
+		end
+		if not hasParams then
+			savedParams = nil
+		end
+	end
 	table.insert(PersistentVars.ObjectLoopEffects[uuid], {
 		Effect = effect,
-		Bone = bone,
-		Handle = handle
+		Params = savedParams
 	})
 end
 
@@ -36,7 +85,7 @@ end
 ---@param effect string
 ---@param handle integer
 ---@param params EffectManagerEsvEffectParams|nil
-function EffectManager.SaveWorldEffectData(target, effect, handle, params)
+function _INTERNAL.SaveWorldEffectData(target, effect, handle, params)
 	if type(handle) ~= "number" then
 		return
 	end
@@ -59,7 +108,7 @@ function EffectManager.SaveWorldEffectData(target, effect, handle, params)
 end
 
 ---@private
-EffectManager.Callbacks = {
+_INTERNAL.Callbacks = {
 	LoopEffectStarted = {}
 }
 
@@ -68,7 +117,7 @@ EffectManager.Callbacks = {
 EffectManager.Register = {
 	---@param callback EffectManagerLoopEffectStartedCallback
 	LoopEffectStarted = function(callback)
-		table.insert(EffectManager.Callbacks.TagObject, callback)
+		table.insert(_INTERNAL.Callbacks.TagObject, callback)
 	end
 }
 
@@ -98,22 +147,184 @@ local function PrepareOptions(tbl)
 	return opts
 end
 
----@param target LeaderLibEffectManagerTarget Either an object or a position.
----@param effect string
----@param bone string|nil If playing an effect on an object, this is an optional bone name to use.
-function EffectManager.StartLoopEffect(target, effect, bone)
-	local handle = nil
+---@class EffectManagerPlayEffectResult
+---@field ID string
+---@field Handle integer
+---@field Effect EffectManagerEsvEffect|string
+---@field Position number[]|nil
 
-	if type(target) == "table" then
-		handle = PlayLoopEffectAtPosition(effect, target[1], target[2], target[3])
-		EffectManager.SaveWorldEffectData(target, effect, handle)
-	else
-		bone = bone or ""
-		local uuid = GameHelpers.GetUUID(target)
-		handle = PlayLoopEffect(uuid, effect, bone)
-		EffectManager.SaveObjectEffectData(uuid, effect, bone, handle)
+---@return EffectManagerPlayEffectResult
+local function CreateEffectResult(effect, handle, id, pos)
+	return {Effect = effect, Handle = handle, ID = id, Position = pos}
+end
+
+---@param fx string|string[] The effect resource name
+---@param object CharacterParam|ItemParam
+---@param params EffectManagerEsvEffectParams|nil
+---@return EffectManagerPlayEffectResult|EffectManagerPlayEffectResult[]
+function _INTERNAL.PlayEffect(fx, object, params)
+	local t = type(fx)
+	assert(t == "string" or t == "table", "fx parameter must be a string or a table of strings.")
+	local object = GameHelpers.TryGetObject(object)
+	assert(type(object) == "userdata", "object parameter must be a UUID, NetID, or EsvCharacter/EsvItem.")
+	if t == "string" then
+		local handle = nil
+		if _EXTVERSION >= 56 then
+			---@type EffectManagerEsvEffect
+			local effect = Ext.Effect.CreateEffect(fx, object.Handle, params and params.Bone or "")
+			handle = Ext.HandleToDouble(effect.Handle)
+			if params and type(params) == "table" then
+				for k,v in pairs(params) do
+					if ObjectHandleEffectParams[k] then
+						local obj = GameHelpers.TryGetObject(v)
+						if obj then
+							effect[k] = obj.Handle
+						end
+					else
+						effect[k] = v
+					end
+				end
+				if params.Loop then
+					InvokeListenerCallbacks(_INTERNAL.Callbacks.LoopEffectStarted, effect, object, handle, params.Bone or "")
+				end
+			end
+			return CreateEffectResult(effect, handle, effect.EffectName)
+		elseif Ext.OsirisIsCallable() then
+			if params then
+				if params.BeamTarget then
+					handle = PlayLoopBeamEffect(object.MyGuid, params.BeamTarget.MyGuid, fx, params.Bone or "", params.BeamTargetBone or "")
+				else
+					handle = PlayLoopEffect(object.MyGuid, fx, params.Bone or "")
+				end
+			else
+				handle = PlayLoopEffect(object.MyGuid, fx, "")
+			end
+			return CreateEffectResult(fx, handle, fx)
+		end
+	elseif t == "table" then
+		local results = {}
+		for _,v in pairs(fx) do
+			local result = {_INTERNAL.PlayEffect(v, object, params)}
+			if result ~= nil then
+				results[#results+1] = result
+			end
+		end
+		return results
 	end
-	InvokeListenerCallbacks(EffectManager.Callbacks.LoopEffectStarted, effect, target, handle, bone)
+end
+
+---@param fx string|string[] The effect resource name
+---@param pos number[]|EsvGameObject
+---@param params EffectManagerEsvEffectParams|nil
+---@return EffectManagerPlayEffectResult|EffectManagerPlayEffectResult[]
+function _INTERNAL.PlayEffectAt(fx, pos, params)
+	local t = type(fx)
+	assert(t == "string" or t == "table", "Effect parameter must be a string or a table of strings.")
+	if t == "string" then
+		local pt = type(pos)
+		local x,y,z = nil,nil,nil
+		if pt == "table" then
+			x,y,z = table.unpack(pos)
+		elseif pt == "userdata" and pos.WorldPos then
+			x,y,z = table.unpack(pos.WorldPos)
+		end
+		assert(x and y and z, "Position table is invalid - {x,y,z} required.")
+		if _EXTVERSION >= 56 then
+			local handle = nil
+			---@type EffectManagerEsvEffect
+			local effect = Ext.Effect.CreateEffect(fx, Ext.Entity.NullHandle(), "")
+			if effect then
+				effect.Position[1] = x
+				effect.Position[2] = y
+				effect.Position[3] = z
+			end
+			if type(params) == "table" then
+				for k,v in pairs(params) do
+					if ObjectHandleEffectParams[k] then
+						local obj = GameHelpers.TryGetObject(v)
+						if obj then
+							effect[k] = obj.Handle
+						end
+					else
+						effect[k] = v
+					end
+				end
+				if params.Loop then
+					handle = Ext.HandleToDouble(effect.Handle)
+				end
+			end
+			return CreateEffectResult(effect, handle, effect.EffectName, {x,y,z})
+		elseif Ext.OsirisIsCallable() then
+			local handle = nil
+			if params then
+				if params.Loop then
+					if params.Scale then
+						handle = PlayScaledLoopEffectAtPosition(fx, params.Scale, x, y, z)
+					else
+						handle = PlayLoopEffectAtPosition(fx, x, y, z)
+					end
+				else
+					if params.Scale then
+						PlayScaledEffectAtPosition(fx, params.Scale, x, y, z)
+					else
+						PlayLoopEffectAtPosition(fx, x, y, z)
+					end
+				end
+			else
+				PlayEffectAtPosition(fx, x, y, z)
+			end
+			return CreateEffectResult(fx, handle, fx, {x,y,z})
+		end
+	elseif t == "table" then
+		local results = {}
+		for _,v in pairs(fx) do
+			local result = EffectManager.PlayEffectAt(v, pos, params)
+			if result ~= nil then
+				results[#results+1] = result
+			end
+		end
+		return results
+	end
+end
+
+---@param fx string|string[] The effect resource name
+---@param object CharacterParam|ItemParam
+---@param params EffectManagerEsvEffectParams|nil
+---@return EffectManagerPlayEffectResult|EffectManagerPlayEffectResult[]
+function EffectManager.PlayEffect(fx, object, params)
+	local uuid = GameHelpers.GetUUID(object)
+	local result = _INTERNAL.PlayEffect(fx, object, params)
+	if result and params and params.Loop == true then
+		if type(result) == "table" then
+			for i,v in pairs(result) do
+				_INTERNAL.SaveObjectEffectData(uuid, v.ID, v.Handle, params)
+			end
+		else
+			_INTERNAL.SaveObjectEffectData(uuid, result.ID, result.Handle, params)
+		end
+	end
+	return result
+end
+
+---Play an effect at a position with optional parameters (looped, scaled).
+---Returns the EsvEffect if v56 or higher, otherwise it returns a handle.
+---If fx is a table of effects, a table or EsvEffect or table of handles will be returned.
+---@param fx string|string[] The effect resource name
+---@param pos number[]|EsvGameObject
+---@param params EffectManagerEsvEffectParams|nil
+---@return EffectManagerPlayEffectResult|EffectManagerPlayEffectResult[]
+function EffectManager.PlayEffectAt(fx, pos, params)
+	local result = _INTERNAL.PlayEffectAt(fx, pos, params)
+	if result and params and params.Loop == true then
+		if type(result) == "table" then
+			for i,v in pairs(result) do
+				_INTERNAL.SaveWorldEffectData(v.Position, v.ID, v.Handle, params)
+			end
+		else
+			_INTERNAL.SaveWorldEffectData(v.Position, result.ID, result.Handle, params)
+		end
+	end
+	return result
 end
 
 ---@param handle integer
@@ -229,117 +440,30 @@ function EffectManager.RestoreEffects(region)
 		end
 	end
 
-	--TODO
-	-- for uuid,dataTable in pairs(PersistentVars.ObjectLoopEffects) do
-	-- 	for i,v in pairs(dataTable) do
-	-- 		if v.Effect == effect then
-	-- 			StopLoopEffect(v.Handle)
-	-- 			table.remove(dataTable, i)
-	-- 			if not options.all then
-	-- 				break
-	-- 			end
-	-- 		end
-	-- 	end
-	-- 	if #dataTable == 0 then
-	-- 		PersistentVars.ObjectLoopEffects[uuid] = nil
-	-- 	end
-	-- end
+	for uuid,dataTable in pairs(PersistentVars.ObjectLoopEffects) do
+		if ObjectExists(uuid) == 1 and #dataTable > 0 then
+			for i,v in pairs(dataTable) do
+				StopLoopEffect(v.Handle)
+				v.Handle = PlayLoopEffect(uuid, v.Effect, v.Bone)
+				if not v.Handle then
+					table.remove(dataTable, i)
+				end
+			end
+			if #dataTable == 0 then
+				PersistentVars.ObjectLoopEffects[uuid] = nil
+			end
+		else
+			PersistentVars.ObjectLoopEffects[uuid] = nil
+		end
+	end
 end
 
----@class EffectManagerEsvEffectParams
----@field BeamTarget ObjectHandle
----@field BeamTargetBone string
----@field BeamTargetPos number[]
----@field Bone string
----@field DetachBeam boolean
----@field Duration number
----@field EffectName string
----@field ForgetEffect boolean
----@field IsDeleted boolean
----@field IsForgotten boolean
----@field Loop boolean
----@field Position number[]
----@field Rotation number[]
----@field Scale number
----@field Target ObjectHandle
-
----@class EffectManagerEsvEffect:EffectManagerEsvEffectParams
----@field NetID NETID
----@field Delete fun(self:EffectManagerEsvEffect)
-
----Play an effect at a position with optional parameters (looped, scaled).
----Returns the EsvEffect if v56 or higher, otherwise it returns a handle.
----If fx is a table of effects, a table or EsvEffect or table of handles will be returned.
----@param fx string|string[] The effect resource name
----@param pos number[]|EsvGameObject
----@param effectParams EffectManagerEsvEffectParams|nil
----@return EffectManagerEsvEffect|EffectManagerEsvEffect[]|integer|integer[]
-function EffectManager.PlayEffectAt(fx, pos, effectParams)
-	local t = type(fx)
-	assert(t == "string" or t == "table", "Effect parameter must be a string or a table of strings.")
-	if t == "string" then
-		local pt = type(pos)
-		local x,y,z = nil,nil,nil
-		if pt == "table" then
-			x,y,z = table.unpack(pos)
-		elseif pt == "userdata" and pos.WorldPos then
-			x,y,z = table.unpack(pos.WorldPos)
+function EffectManager.DeleteLoopEffects(region)
+	PersistentVars.WorldLoopEffects[region] = nil
+	for uuid,dataTable in pairs(PersistentVars.ObjectLoopEffects) do
+		if ObjectExists(uuid) == 0 or ObjectIsGlobal(uuid) == 0 then
+			PersistentVars.ObjectLoopEffects[uuid] = nil
 		end
-		assert(x and y and z, "Position table is invalid - {x,y,z} required.")
-		if _EXTVERSION >= 56 then
-			---@type EffectManagerEsvEffect
-			local effect = Ext.Effect.CreateEffect(fx, Ext.Entity.NullHandle(), "")
-			if effect then
-				effect.Position[1] = x
-				effect.Position[2] = y
-				effect.Position[3] = z
-			end
-			if type(effectParams) == "table" then
-				for k,v in pairs(effectParams) do
-					if effect[k] then
-						effect[k] = v
-					end
-				end
-				if effectParams.Loop then
-					local handle = Ext.HandleToDouble(effect.Handle)
-					EffectManager.SaveWorldEffectData({x,y,z}, fx, handle, effectParams)
-				end
-			end
-			return effect
-		elseif Ext.OsirisIsCallable() then
-			if effectParams then
-				if effectParams.Loop then
-					if effectParams.Scale then
-						local handle = PlayScaledLoopEffectAtPosition(fx, effectParams.Scale, x, y, z)
-						EffectManager.SaveWorldEffectData({x,y,z}, fx, handle)
-						return handle
-					else
-						local handle = PlayLoopEffectAtPosition(fx, x, y, z)
-						EffectManager.SaveWorldEffectData({x,y,z}, fx, handle, effectParams)
-						return handle
-					end
-				else
-					if effectParams.Scale then
-						PlayScaledEffectAtPosition(fx, effectParams.Scale, x, y, z)
-						return true
-					else
-						PlayLoopEffectAtPosition(fx, x, y, z)
-						return true
-					end
-				end
-			else
-				PlayEffectAtPosition(fx, x, y, z)
-			end
-		end
-	elseif t == "table" then
-		local results = {}
-		for _,v in pairs(fx) do
-			local result = EffectManager.PlayEffectAt(v, pos, effectParams)
-			if result ~= nil then
-				results[#results+1] = result
-			end
-		end
-		return results
 	end
 end
 

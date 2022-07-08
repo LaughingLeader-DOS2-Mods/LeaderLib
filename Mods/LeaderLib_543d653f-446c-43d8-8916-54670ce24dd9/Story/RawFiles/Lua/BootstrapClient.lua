7dbeb5ffede474ec7c780c79b70163d6a9c35410
LaughingLeader <laughingleader@gmail.com>
2022-07-07 19:22:20 -0500

    Check _totalVisibleSlots's type

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/BootstrapClient.lua
@@ -1,7 +1,7 @@
 Ext.Require("BootstrapShared.lua")
 
 if UI == nil then
-	---@class LeaderLibUIHelpers
+	---@class LeaderLibUIMain
 	UI = {}
 end
 
@@ -20,7 +20,7 @@ Events.LuaReset:Subscribe(function ()
 	local ui = Ext.GetUIByType(not Vars.ControllerEnabled and Data.UIType.hotBar or Data.UIType.bottomBar_c)
 	if ui then
 		local this = ui:GetRoot()
-		if this and this._totalVisibleSlots then
+		if this and type(this._totalVisibleSlots) == "number" then
 			UI.MaxHotbarSlots = this._totalVisibleSlots
 		end
 	end

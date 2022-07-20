dcad1898587e5cd258a73aebf999c37dcb84e760
LaughingLeader <laughingleader@gmail.com>
2022-07-20 11:01:47 -0500

    Add an option to set base carry weight

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Settings/LeaderLibDefaultGlobalSettings.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Settings/LeaderLibDefaultGlobalSettings.lua
@@ -28,6 +28,7 @@ settings.Global.Flags.LeaderLib_RemovePathInfluencesOnChainAll.DebugOnly = true
 --settings.Global:AddLocalizedVariable("AutosaveInterval", "LeaderLib_Variables_AutosaveInterval", 15, 1, 600, 1)
 settings.Global:AddLocalizedVariable("AutoCombatRange", "LeaderLib_Variables_AutoCombatRange", 30, 1, 30, 1)
 settings.Global:AddLocalizedVariable("CombatSightRangeMultiplier", "LeaderLib_Variables_CombatSightRangeMultiplier", 2.5, 1, 30, 1)
+settings.Global:AddLocalizedVariable("CarryWeightBase", "LeaderLib_Variables_CarryWeightBase", 0, 0, 200000, 1)
 
 settings.GetMenuOrder = function()
 	local order = {
@@ -43,10 +44,11 @@ settings.GetMenuOrder = function()
 		Entries = {
 			"LeaderLib_AutoAddModMenuBooksDisabled",
 			"LeaderLib_AutoUnlockInventoryInMultiplayer",
-			"LeaderLib_AllTooltipsForItemsEnabled",
 			"LeaderLib_AutoIdentifyItemsEnabled",
+			"CarryWeightBase",
 			"LeaderLib_PermanentSpiritVisionEnabled",
 			"LeaderLib_ShowConsumableEffectsEnabled",
+			"LeaderLib_AllTooltipsForItemsEnabled",
 			"LeaderLib_RemovePathInfluencesOnChainAll"
 		}},
 		{DisplayName = GameHelpers.GetStringKeyText("LeaderLib_UI_Settings_DialogRedirection", "Dialog Redirection"),
@@ -99,16 +101,46 @@ end
 
 GlobalSettings.Mods[ModuleUUID] = settings
 
+--round(((1 (or 2.0 if Packmule talent) * Strength) * [ExtraData:CarryWeightPerStr:10000]) + CarryWeightBase)
+local _weightFormulaText = "round((Packmule:%s x Strength:%s) x CarryWeightPerStr:%s) + CarryWeightBase:<font color='#33DD33'>%s</font>) = <font color='#33FF33'>%s</font>"
+
+Events.GetTextPlaceholder:Subscribe(function (e)
+	--round(((1 (or 2.0 if Packmule talent) * Strength) * [ExtraData:CarryWeightPerStr:10000]) + CarryWeightBase)
+	local packMuleBonus = e.Character.TALENT_Carry and 2.0 or 1.0
+	local strength = e.Character.Strength
+	local weightPerStr = GameHelpers.GetExtraData("CarryWeightPerStr", 10000)
+	local carryWeightBase = GameHelpers.GetExtraData("CarryWeightBase", 0)
+	local result = Ext.Round(((packMuleBonus * strength) * weightPerStr) + carryWeightBase)
+	return _weightFormulaText:format(packMuleBonus, strength, weightPerStr, carryWeightBase, result)
+
+end, {MatchArgs={ID="CarryWeightFormula"}})
+
 Ext.Events.SessionLoaded:Subscribe(function (e)
 	local sightRange = GameHelpers.GetExtraData("End Of Combat SightRange Multiplier", 2.5)
 	if sightRange ~= 2.5 then
 		settings.Global.Variables.CombatSightRangeMultiplier.Default = sightRange
 	end
+	local carryWeightBase = GameHelpers.GetExtraData("CarryWeightBase", 2.5)
+	if carryWeightBase ~= 0 then
+		settings.Global.Variables.CarryWeightBase.Default = carryWeightBase
+	end
+
+end)
+
+Events.BeforeLuaReset:Subscribe(function (e)
+	Ext.ExtraData["End Of Combat SightRange Multiplier"] = settings.Global.Variables.CombatSightRangeMultiplier.Default
+	Ext.ExtraData.CarryWeightBase = settings.Global.Variables.CarryWeightBase.Default
 end)
 
 settings.Global.Variables.CombatSightRangeMultiplier:Subscribe(function (e)
 	Ext.ExtraData["End Of Combat SightRange Multiplier"] = e.Value
 	--Ext.ExtraData["Ally Joins Ally SightRange Multiplier"] = e.Value -- Unused?
+	fprint(LOGLEVEL.TRACE, "[LeaderLib] Set 'End Of Combat SightRange Multiplier' to (%s)", e.Value)
+end)
+
+settings.Global.Variables.CarryWeightBase:Subscribe(function (e)
+	Ext.ExtraData.CarryWeightBase = e.Value
+	fprint(LOGLEVEL.TRACE, "[LeaderLib] Set CarryWeightBase to (%s)", e.Value)
 end)
 
 if Ext.IsServer() then

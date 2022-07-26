510c34c4b22ab4afc0f3fd067823ff8e715ced07
LaughingLeader <laughingleader@gmail.com>
2022-07-26 12:25:38 -0500

    Setup auto-completion for Mods.LeaderLib

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/_Reference/AutoCompletion.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/_Reference/AutoCompletion.lua
@@ -3,12 +3,16 @@
 --This file is never actually loaded, and is used to make EmmyLua work better.
 
 if not Mods then Mods = {} end
-if not Mods.LeaderLib then Mods.LeaderLib = {} end
-if not Mods.LeaderLib.Listeners then
-	Mods.LeaderLib.Listeners = {}
-end
-if not Mods.LeaderLib.Import then
-	Mods.LeaderLib.Import = Import
+
+---@class LeaderLibModTable
+if not Mods.LeaderLib 
+	then Mods.LeaderLib = {
+		Import = Import,
+		GameHelpers = GameHelpers,
+		Common = Common,
+		StringHelpers = StringHelpers,
+		TableHelpers = TableHelpers
+	}
 end
 
 ---@alias DamageType string|"None"|"Physical"|"Piercing"|"Corrosive"|"Magic"|"Chaos"|"Fire"|"Air"|"Water"|"Earth"|"Poison"|"Shadow"

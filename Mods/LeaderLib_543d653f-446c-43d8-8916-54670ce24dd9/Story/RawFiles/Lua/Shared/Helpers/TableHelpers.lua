a4bf5582e5086595e4767a8c1ab5133597a9c8aa
LaughingLeader <laughingleader@gmail.com>
2022-10-28 10:25:32 -0500

    Return pairs in TableHelpers.TryOrderedEach for userdata

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Helpers/TableHelpers.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Helpers/TableHelpers.lua
@@ -31,20 +31,24 @@ end
 
 ---Returns an ordered iterator if the table is structured like that, otherwise returns a regular next iterator.
 function TableHelpers.TryOrderedEach(tbl)
-	local len = tbl and #tbl or 0
-	if len > 0 then
-		local i = 0
-		return function ()
-			i = i + 1
-			if i <= len then
-				return i,tbl[i]
+	if type(tbl) == "table" then
+		local len = tbl and #tbl or 0
+		if len > 0 then
+			local i = 0
+			return function ()
+				i = i + 1
+				if i <= len then
+					return i,tbl[i]
+				end
 			end
+		else
+			local function iter(tbl, index)
+				return next(tbl, index)
+			end
+			return iter,tbl,nil
 		end
 	else
-		local function iter(tbl, index)
-			return next(tbl, index)
-		end
-		return iter,tbl,nil
+		return pairs(tbl)
 	end
 end
 

7c4bec19ecb9e2dfaaa7020b8090c33b81a72369
LaughingLeader <laughingleader@gmail.com>
2022-10-28 10:25:13 -0500

    Update local and cached root templates when updating world tooltips
    Fixes the issue of items not being able to stack with each other if the item has a local template

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/QOL/WorldTooltips.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/QOL/WorldTooltips.lua
@@ -1,4 +1,5 @@
 local _EXTVERSION = Ext.Utils.Version()
+local _ISCLIENT = Ext.IsClient()
 
 local WorldTooltips = {
 	TooltipMode = 2, -- World and Hover
@@ -10,15 +11,51 @@ function WorldTooltips:IsEnabled()
 	return GameHelpers.IsLevelType(LEVELTYPE.GAME) and SettingsManager.GetMod(ModuleUUID).Global:FlagEquals("LeaderLib_AllTooltipsForItemsEnabled", true)
 end
 
-if Ext.IsClient() then
+---@param templates table<FixedString,boolean>
+local function _UpdateTemplates(templates)
+	local level = Ext.Entity.GetCurrentLevel()
+	if level then
+		local levelTemplates = level.LocalTemplateManager.Templates
+		local cachedTemplates = level.LevelCacheTemplateManager.Templates
+		for id,b in pairs(templates) do
+			local template = levelTemplates[id]
+			---@cast template ItemTemplate
+			if template then
+				template.Tooltip = WorldTooltips.TooltipMode
+			end
+			template = cachedTemplates[id]
+			if template then
+				template.Tooltip = WorldTooltips.TooltipMode
+			end
+			template = Ext.Template.GetRootTemplate(id)
+			if template then
+				template.Tooltip = WorldTooltips.TooltipMode
+			end
+		end
+	else
+		for id,b in pairs(templates) do
+			local template = Ext.Template.GetRootTemplate(id)
+			if template then
+				template.Tooltip = WorldTooltips.TooltipMode
+			end
+		end
+	end
+end
+
+if _ISCLIENT then
 	function WorldTooltips:UpdateItems(cmd, payload)
-		local ids = Common.JsonParse(payload)
-		if ids then
-			for i=1,#ids do
-				local item = GameHelpers.GetItem(ids[i])
-				if item and item.RootTemplate then
-					item.RootTemplate.Tooltip = WorldTooltips.TooltipMode
+		local data = Common.JsonParse(payload)
+		if data then
+			if data.Items then
+				for i=1,#data.Items do
+					local item = GameHelpers.GetItem(data.Items[i])
+					if item and item.CurrentTemplate then
+						item.CurrentTemplate.Tooltip = WorldTooltips.TooltipMode
+					end
 				end
+			end
+			if data.Templates then
+				_UpdateTemplates(data.Templates)
 			end
 		end
 	end
@@ -27,7 +64,7 @@ else
 	---@param item EsvItem
 	---@param force boolean|nil Skip the Tooltip ~= 2 check to update the client.
 	local function _ShouldHaveTooltip(item, force)
-		if item.RootTemplate and (item.RootTemplate.Tooltip ~= WorldTooltips.TooltipMode or force) and not StringHelpers.IsNullOrWhitespace(GameHelpers.GetDisplayName(item)) then
+		if item.CurrentTemplate and (item.CurrentTemplate.Tooltip ~= WorldTooltips.TooltipMode or force) and not StringHelpers.IsNullOrWhitespace(GameHelpers.GetDisplayName(item)) then
 			return true
 		end
 		return false
@@ -35,22 +72,14 @@ else
 
 	---@return EsvItem[]
 	local function _GetAllItems()
-		local items = {}
-		if _EXTVERSION < 57 then
-			for i,v in pairs(Ext.Entity.GetAllItemGuids()) do
-				local item = GameHelpers.GetItem(v)
-				if item then
-					items[#items+1] = item
-				end
-			end
-			return items
-		else
-			local level = Ext.Entity.GetCurrentLevel()
-			if level then
-				return level.EntityManager.ItemConversionHelpers.RegisteredItems[level.LevelDesc.LevelName] or items
+		local level = Ext.Entity.GetCurrentLevel()
+		if level then
+			local items = level.EntityManager.ItemConversionHelpers.RegisteredItems[level.LevelDesc.LevelName]
+			if items then
+				return items
 			end
 		end
-		return items
+		return {}
 	end
 
 	local _ValidUpdateStates = {
@@ -67,18 +96,24 @@ else
 		if _ValidUpdateStates[Ext.GetGameState()] and WorldTooltips:IsEnabled() then
 			local time = Ext.Utils.MonotonicTime()
 			local updateDataLen = 0
-			local updateData = {}
+			local updateData = {
+				Items = {},
+				Templates = {},
+			}
 			for _,item in pairs(_GetAllItems()) do
 				if _ShouldHaveTooltip(item, forceResync) then
 					updateDataLen = updateDataLen + 1
-					updateData[updateDataLen] = item.NetID
-					item.RootTemplate.Tooltip = WorldTooltips.TooltipMode
+					updateData.Items[updateDataLen] = item.NetID
+					local template = GameHelpers.GetTemplate(item)
+					updateData.Templates[template] = true
+					item.CurrentTemplate.Tooltip = WorldTooltips.TooltipMode
 				end
 			end
 			if updateDataLen > 0 then
 				if Vars.LeaderDebugMode then
 					fprint(LOGLEVEL.DEFAULT, "[LeaderLib:WorldTooltips.UpdateWorldItems] World tooltip updating took (%s) ms.", Ext.Utils.MonotonicTime()-time)
 				end
+				_UpdateTemplates(updateData.Templates)
 				GameHelpers.Net.Broadcast("LeaderLib_WorldTooltips_UpdateClient", updateData)
 			end
 		end
@@ -87,8 +122,9 @@ else
 	---@param item EsvItem
 	function WorldTooltips:OnItemEnteredWorld(item)
 		if item and item.CurrentLevel ~= "" and _ShouldHaveTooltip(item) then
-			item.RootTemplate.Tooltip = WorldTooltips.TooltipMode
-			GameHelpers.Net.Broadcast("LeaderLib_WorldTooltips_UpdateClient", {item.NetID})
+			item.CurrentTemplate.Tooltip = WorldTooltips.TooltipMode
+			local template = GameHelpers.GetTemplate(item)
+			GameHelpers.Net.Broadcast("LeaderLib_WorldTooltips_UpdateClient", {Items={item.NetID}, Templates={[template]=true}})
 		end
 	end
 

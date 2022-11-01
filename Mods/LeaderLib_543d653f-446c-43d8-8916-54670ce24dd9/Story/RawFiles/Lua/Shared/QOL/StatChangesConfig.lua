9f21e8dc286f9b1806a7afa58302b58301631233
LaughingLeader <laughingleader@gmail.com>
2022-11-01 11:49:05 -0500

    Add support for editing Data.txt keys in StatChangesConfig

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/QOL/StatChangesConfig.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/QOL/StatChangesConfig.lua
@@ -16,24 +16,66 @@ local _validSyncStates = {
 	GameMasterPause = true,
 }
 
+local function _ParseSpecialKey(id, tbl)
+	if id == "Data" or id == "ExtraData" then
+		local extraData = Ext.ExtraData
+		for k,v in pairs(tbl) do
+			if extraData[k] then
+				local t1 = type(v)
+				local t2 = type(extraData[k])
+				if t1 ~= t2 then
+					fprint(LOGLEVEL.WARNING, "[LeaderLib:StatChangesConfig] Data key (%s) type (%s) does not equal the type in LeaderLib_StatChanges (%s). This may lead to issues.", k, t1, t2)
+					if t2 == "string" then
+						local x = tonumber(v)
+						if x then
+							extraData[k] = x
+						end
+					end
+				else
+					extraData[k] = v
+				end
+			else
+				extraData[k] = v
+			end
+		end
+		return true
+	elseif id == "TreasureTable" then
+		if not _ISCLIENT then
+			for tableID,attributes in pairs(tbl) do
+				local tt = Ext.Stats.TreasureTable.GetLegacy(tableID)
+				if tt then
+					TableHelpers.AddOrUpdate(tt, attributes, false, true)
+					Ext.Stats.TreasureTable.Update(tt)
+				end
+			end
+		end
+		return true
+	end
+	return false
+end
+
 function StatChangesConfig:_Apply()
 	local doSync = not _ISCLIENT and _validSyncStates[Ext.Server.GetGameState()]
 	for id,attributes in pairs(self._Data) do
-		if GameHelpers.Stats.Exists(id) then
-			local stat = Ext.Stats.Get(id, nil, false)
-			fprint(LOGLEVEL.TRACE, "[LeaderLib:StatChangesConfig] Modifying stat (%s):", id)
-			for k,v in pairs(attributes) do
-				fprint(LOGLEVEL.TRACE, "  [%s] = %s", k, Lib.serpent.line(v, {comment=false}))
-				local b,err = xpcall(_TrySetStat, debug.traceback, stat, k, v)
-				if not b then
-					fprint(LOGLEVEL.ERROR, "[LeaderLib:StatChangesConfig] Error setting attribute (%s) for stat (%s)':\n%s", k, id, err)
+		if not _ParseSpecialKey(id, attributes) then
+			if GameHelpers.Stats.Exists(id) then
+				local stat = Ext.Stats.Get(id, nil, false)
+				if stat then
+					fprint(LOGLEVEL.TRACE, "[LeaderLib:StatChangesConfig] Modifying stat (%s):", id)
+					for k,v in pairs(attributes) do
+						fprint(LOGLEVEL.TRACE, "  [%s] = %s", k, Lib.serpent.line(v, {comment=false}))
+						local b,err = xpcall(_TrySetStat, debug.traceback, stat, k, v)
+						if not b then
+							fprint(LOGLEVEL.ERROR, "[LeaderLib:StatChangesConfig] Error setting attribute (%s) for stat (%s)':\n%s", k, id, err)
+						end
+					end
+					if doSync then
+						Ext.Stats.Sync(id, false)
+					end
 				end
+			else
+				fprint(LOGLEVEL.WARNING, "[LeaderLib:StatChangesConfig] Stat (%s) does not exist. Skipping.", id)
 			end
-			if doSync then
-				Ext.Stats.Sync(id, false)
-			end
-		else
-			fprint(LOGLEVEL.WARNING, "[LeaderLib:StatChangesConfig] Stat (%s) does not exist. Skipping.", id)
 		end
 	end
 end

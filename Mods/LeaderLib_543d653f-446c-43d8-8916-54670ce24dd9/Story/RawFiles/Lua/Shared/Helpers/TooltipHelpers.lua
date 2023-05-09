cf17431de97c25ae8eec30e47c84fd17c8f3fe49
LaughingLeader <laughingleader@gmail.com>
2023-05-08 21:09:27 -0500

    Safeguard damageRange type

--- Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Helpers/TooltipHelpers.lua
+++ Mods/LeaderLib_543d653f-446c-43d8-8916-54670ce24dd9/Story/RawFiles/Lua/Shared/Helpers/TooltipHelpers.lua
@@ -527,27 +527,27 @@ end
 ---@param damageRange table<DamageType,{Min:integer, Max:integer}>
 ---@return string
 function GameHelpers.Tooltip.FormatDamageRange(damageRange)
-	if damageRange ~= nil then
-		local damageTexts = {}
-		local totalDamageTypes = 0
-		for damageType,damage in pairs(damageRange) do
-			local min = damage.Min or damage[1]
-			local max = damage.Max or damage[2]
-			if min ~= nil and max ~= nil then
-				if max == min then
-					table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i", max)))
-				else
-					table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i-%i", min, max)))
-				end
-				totalDamageTypes = totalDamageTypes + 1
-			end
-		end
-		if totalDamageTypes > 0 then
-			if totalDamageTypes > 1 then
-				return StringHelpers.Join(", ", damageTexts)
+	local t = type(damageRange)
+	assert(t == "table", ("damageRange must be a table. Type is (%s)"):format(t))
+	local damageTexts = {}
+	local totalDamageTypes = 0
+	for damageType,damage in pairs(damageRange) do
+		local min = damage.Min or damage[1]
+		local max = damage.Max or damage[2]
+		if min ~= nil and max ~= nil then
+			if max == min then
+				table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i", max)))
 			else
-				return damageTexts[1]
+				table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i-%i", min, max)))
 			end
+			totalDamageTypes = totalDamageTypes + 1
+		end
+	end
+	if totalDamageTypes > 0 then
+		if totalDamageTypes > 1 then
+			return StringHelpers.Join(", ", damageTexts)
+		else
+			return damageTexts[1]
 		end
 	end
 	return ""
